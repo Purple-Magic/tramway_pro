980e656251e754808754be180f94177410966bdd
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-05-29 17:13:29 +0400

    updates

--- lib/concerns/controllers/tramway/admin/application_concern.rb
+++ lib/concerns/controllers/tramway/admin/application_concern.rb
@@ -1,16 +1,16 @@
 module Concerns::Controllers
   module Tramway::Admin::ApplicationConcern
-    def available_models_given_with_permissions?
-      constraint = ::Constraints::DomainConstraint.new(ENV['PROJECT_URL'])
-      ::Tramway::Admin.available_models(permission: constraint.engine_loaded) &&
-        params[:model].in?(::Tramway::Admin.available_models(constraint.engine_loaded).map(&:to_s))
-    end
-
-    def singleton_models_given_with_permissions?
-      constraint = ::Constraints::DomainConstraint.new(ENV['PROJECT_URL'])
-      ::Tramway::Admin.singleton_models(permission: constraint.engine_loaded) &&
-        params[:model].in?(::Tramway::Admin.singleton_models(constraint.engine_loaded).map(&:to_s))
-    end
+#    def available_models_given_with_permissions?
+#      constraint = ::Constraints::DomainConstraint.new(ENV['PROJECT_URL'])
+#      ::Tramway::Admin.available_models(permission: constraint.engine_loaded) &&
+#        params[:model].in?(::Tramway::Admin.available_models(constraint.engine_loaded).map(&:to_s))
+#    end
+#
+#    def singleton_models_given_with_permissions?
+#      constraint = ::Constraints::DomainConstraint.new(ENV['PROJECT_URL'])
+#      ::Tramway::Admin.singleton_models(permission: constraint.engine_loaded) &&
+#        params[:model].in?(::Tramway::Admin.singleton_models(constraint.engine_loaded).map(&:to_s))
+#    end
   end
 end
 
