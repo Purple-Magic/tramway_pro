94664449998ce8a63b945279d7a27b95f9dd66fa
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-05-29 17:04:20 +0400

    Fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
 end
-content_for(:admin_navbar) do
-  link_to @application&.title || t('application.title'), try(:root_path) || '/', class: 'navbar-brand'
-end
+#content_for(:admin_navbar) do
+#  link_to @application&.title || t('application.title'), try(:root_path) || '/', class: 'navbar-brand'
+#end
