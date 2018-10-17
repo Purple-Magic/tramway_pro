611116c557a5ebeea6bdc1caf3294e992f8b087f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-10-18 01:36:29 +0400

    Fix exporter

--- bin/export_it_way_participants.rb
+++ bin/export_it_way_participants.rb
@@ -1,8 +1,15 @@
 require 'csv'
 
 CSV.open('list.csv', 'w') do |csv|
-  Tramway::Event::Event.find(3).participants.active.where(participation_state: :prev_approved).each do |p|
-    full_name = "#{p.values['Фамилия']} #{p.values['Имя']} #{p.values['Отчество']}"
-    csv << [ full_name, p.values['Телефон'], p.values['Ссылка на страницу ВКонтакте'], p.values['Населённый пункт'], p.values['Направление на Форуме (подробности появятся позже на этой странице ниже)']]
+  participants = Tramway::Event::Event.find(3).participants.active.where(participation_state: :prev_approved)
+  participants.each_with_index do |p, index|
+    full_name = "#{p.values['Фамилия'].strip} #{p.values['Имя'].strip} #{p.values['Отчество'].strip}"
+    csv << [ full_name,
+             p.values['Телефон'],
+             p.values['Ссылка на страницу ВКонтакте'],
+             p.values['Населённый пункт'],
+             p.values['Направление на Форуме (подробности появятся позже на этой странице ниже)'],
+             p.values['Дата рождения']
+    ]
   end
 end
