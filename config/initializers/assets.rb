acad742560460012327c9de009aeb75882b4fc98
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-09-10 04:40:37 +0400

    IT Way map separate files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( *.jpg *.png )
+Rails.application.config.assets.precompile += %w( *.jpg *.png *.js )
