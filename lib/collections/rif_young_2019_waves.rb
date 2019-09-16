c5825e8c866b53620d29b5e5d407b89bdded5be5
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-09-17 03:44:07 +0400

    Update wavs

--- lib/collections/rif_young_2019_waves.rb
+++ lib/collections/rif_young_2019_waves.rb
@@ -4,9 +4,9 @@ module Collections
   class RifYoung2019Waves < ::Tramway::Collection
     class << self
       def list
-        [ '11:00',
-          '13:00',
-          '15:00']
+        [ '11:00 - 12.30',
+          '13:00 - 14.30',
+          '15:00 - 16.30']
       end
     end
   end
