5def7a5776687d92c7ba14ec6ce5fe257bbdb082
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-09-17 01:43:09 +0400

    Added RIF_YOUNG_COLLECTIONS

---
+++ lib/collections/rif_young_2019_waves.rb
@@ -0,0 +1,13 @@
+require 'tramway/collection'
+
+module Collections
+  class RifYoung2019Waves < ::Tramway::Collection
+    class << self
+      def list
+        [ '11:00',
+          '13:00',
+          '15:00']
+      end
+    end
+  end
+end
