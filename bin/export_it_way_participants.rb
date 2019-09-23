0575f68e8378d18e2ba20fdf3bcc37acc66fcd32
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-09-23 06:07:44 +0400

    IT Way Exporting participants

--- bin/export_it_way_participants.rb
+++ bin/export_it_way_participants.rb
@@ -1,15 +1,105 @@
 require 'csv'
 
+participants = Tramway::Event::Participant.active
+participants.reduce([]) do |csv, p|
+end
+
+association = {
+  3 => {
+    birth_date: 4,
+    link: 9,
+    org: 10,
+    where: 15
+  },
+  4 => {
+    org: 19,
+    where: 20,
+    link: 22
+  },
+  5 => {
+    org: 26,
+    where: 27,
+    link: 29
+  },
+  6 => {
+    org: 34,
+    where: 35,
+    link: 36
+  },
+  7 => {
+    org: 41,
+    where: 43,
+    link: 44
+  },
+  8 => {
+    org: 49,
+    link: 50,
+    where: 51
+  },
+  17 => {
+    org: 60,
+    where: 61,
+    link: 62
+  },
+  18 => {
+    org: 76,
+    where: 77,
+    link: 78
+  },
+  21 => {
+    org: 84,
+    where: 85,
+    link: 86
+  },
+  23 => {
+    org: 91,
+    where: 92,
+    link: 93
+  },
+  9 => {
+    org: 98,
+    where: 99,
+    link: 100
+  },
+  24 => {
+    org: 105,
+    where: 106,
+    link: 107
+  },
+  22 => {
+    org: 112,
+    where: 113,
+    link: 114
+  },
+  26 => {
+    org: 121,
+    link: 123,
+    where: 124,
+    birth_date: 120
+  },
+  25 => {
+    org: 132,
+    where: 133,
+    link: 134
+  },
+  27 => {
+    where: 140,
+    org: 142,
+    link: 143
+  },
+}
+
 CSV.open('list.csv', 'w') do |csv|
-  participants = Tramway::Event::Event.find(3).participants.active.where(participation_state: :prev_approved)
-  participants.each_with_index do |p, index|
-    full_name = "#{p.values['Фамилия'].strip} #{p.values['Имя'].strip} #{p.values['Отчество'].strip}"
-    csv << [ full_name,
-             p.values['Телефон'],
-             p.values['Ссылка на страницу ВКонтакте'],
-             p.values['Населённый пункт'],
-             p.values['Направление на Форуме (подробности появятся позже на этой странице ниже)'],
-             p.values['Дата рождения']
-    ]
+  Tramway::Event::Event.active.find_each do |event|
+    next if event.id == 19
+    event.participants.active.find_each do |participant|
+      form_fields = association[event.id]
+      csv << [ event.title ] + [:org, :where, :link, :birth_date].map do |key|
+        if form_fields[key]
+          form_field = event.participant_form_fields.find form_fields[key] 
+          participant.values[form_field.title]
+        end
+      end
+    end
   end
 end
