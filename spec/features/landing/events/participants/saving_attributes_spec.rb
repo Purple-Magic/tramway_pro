b6f9ba0eb5671a504bc57934d040a97a06c9c101
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-03-02 04:42:09 +0400

    Saving params of participants

---
+++ spec/features/landing/events/participants/saving_attributes_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'IT Way: Creating participant' do
+  before { set_host it_way_host }
+  let(:event) { create :event, :campaign_started, project_id: it_way_id }
+  let(:attributes) { attributes_for :participant_default_event_attributes }
+
+  context 'Error' do
+    it 'saving participant data in the form' do
+      visit "/events/#{event.id}"
+
+      fill_in 'tramway_event_participant[Имя]', with: attributes[:'Имя']
+      fill_in 'tramway_event_participant[Место учёбы / работы]', with: attributes[:'Место учёбы / работы']
+      fill_in 'tramway_event_participant[Номер телефона]', with: attributes[:'Номер телефона']
+      fill_in 'tramway_event_participant[Email]', with: attributes[:Email]
+
+      click_on 'Отправить заявку'
+
+      expect(page).to have_field('tramway_event_participant[Имя]', with: attributes[:'Имя'])
+      expect(page).to have_field('tramway_event_participant[Место учёбы / работы]', with: attributes[:'Место учёбы / работы'])
+      expect(page).to have_field('tramway_event_participant[Номер телефона]', with: attributes[:'Номер телефона'])
+      expect(page).to have_field('tramway_event_participant[Email]', with: attributes[:Email])
+    end
+  end
+end
