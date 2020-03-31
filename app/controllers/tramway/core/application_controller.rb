a83bff36c3b4f32ab77722bda6c742920aac26a1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-03-31 15:43:02 +0400

    Fix pages

--- app/controllers/tramway/core/application_controller.rb
+++ app/controllers/tramway/core/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Tramway::Core::ApplicationController < ActionController::Base
+  before_action :application
+
   class << self
     def before_render(method, **options)
       @before_render_methods ||= {}
@@ -18,5 +20,11 @@ class Tramway::Core::ApplicationController < ActionController::Base
       send method
     end
     super
+  end
+
+  # FIXME it's repeat of tramway-core/application_controller method
+
+  def application
+    @application = ::Tramway::Core.application_object
   end
 end
