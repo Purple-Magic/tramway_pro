f913b3ae8954a4c9d9d886e0e6d7d493715693be
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-04-15 03:22:53 +0400

    Working listai page

--- app/controllers/engineervol/web/welcome_controller.rb
+++ app/controllers/engineervol/web/welcome_controller.rb
@@ -1,4 +1,8 @@
 class Engineervol::Web::WelcomeController < ApplicationController
+  layout 'engineervol/application'
+
   def index
+    @application = Constraints::DomainConstraint.new(request.domain).application_object
+    @book = Listai::Book.first
   end
 end
--- app/models/listai/page.rb
+++ app/models/listai/page.rb
@@ -1,3 +1,5 @@
 class Listai::Page < ApplicationRecord
   belongs_to :book, class_name: 'Listai::Book'
+
+  mount_uploader :file, PhotoUploader
 end
