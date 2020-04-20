a1c2ec00ded08d8f6ed20bcd24a0c2a72257f123
moshinaan <moshinahacker@yandex.ru>
2020-04-20 16:22:06 +0400

    #24 add delete test

---
+++ spec/features/admin/events/destroy_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Destroy event' do
+  before { move_host_to it_way_host }
+  before { create :event, project_id: it_way_id }
+
+  it 'should destroy event' do
+    visit '/admin'
+    fill_in 'Email', with: 'admin@email.com'
+    fill_in 'Пароль', with: '123456'
+    click_on 'Войти', class: 'btn-success'
+
+    last_event = Tramway::Event::Event.last
+    click_on_dropdown 'Организация мероприятий'
+    click_on 'Мероприятия'
+    click_on_delete_button last_event
+    last_event.reload
+    
+    expect(last_event.remove?).to be_truthy
+  end
+end
