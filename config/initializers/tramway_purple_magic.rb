15ec2527774205b8a2bd0e97376fc4f7d9341835
moshinaan <moshinahacker@yandex.ru>
2020-04-30 22:46:16 +0400

    #purple-magic  Configurate navbar

--- config/initializers/tramway_purple_magic.rb
+++ config/initializers/tramway_purple_magic.rb
@@ -1,3 +1,14 @@
 Tramway::Core.initialize_application model_class: PurpleMagic
 Tramway::Admin.set_singleton_models PurpleMagic, project: :purple_magic
 Tramway::Auth.root_path = '/admin'
+
+Tramway::Admin.navbar_structure(
+  PurpleMagic,
+  {
+    my_dropdown:
+    [
+      :divider
+    ]
+  },
+  project: :purple_magic
+)
