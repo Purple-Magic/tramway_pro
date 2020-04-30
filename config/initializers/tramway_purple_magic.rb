167dace42cd73c643a56de30e50ca041b299e442
moshinaan <moshinahacker@yandex.ru>
2020-04-30 23:33:47 +0400

    #purple_magic update initializers

--- config/initializers/tramway_purple_magic.rb
+++ config/initializers/tramway_purple_magic.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-
-Tramway::Core.initialize_application model_class: PurpleMagic
 Tramway::Admin.set_singleton_models PurpleMagic, project: :purple_magic
 Tramway::Auth.root_path = '/admin'
 
