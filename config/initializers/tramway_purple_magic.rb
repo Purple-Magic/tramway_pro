7bec9be970726ff78e4eb7c759b2fe63b6783d5c
moshinaan <moshinahacker@yandex.ru>
2020-05-01 14:47:12 +0400

    #23 fix purple magic initializers

--- config/initializers/tramway_purple_magic.rb
+++ config/initializers/tramway_purple_magic.rb
@@ -1,13 +1,2 @@
 Tramway::Admin.set_singleton_models PurpleMagic, project: :purple_magic
 Tramway::Auth.root_path = '/admin'
-
-Tramway::Admin.navbar_structure(
-  PurpleMagic,
-  {
-    my_dropdown:
-    [
-      :divider
-    ]
-  },
-  project: :purple_magic
-)
