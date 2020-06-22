73f36b2d7e6ec55ef90bc8aa696e4a4f86c2be79
moshinaan <moshinahacker@yandex.ru>
2020-06-22 13:42:43 +0400

    #purple magic fix initializers settings

--- app/models/purple_magic/purple_magic.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class PurpleMagic::PurpleMagic < Tramway::Core::ApplicationRecord
-end
--- config/initializers/tramway/purple_magic.rb
+++ config/initializers/tramway/purple_magic.rb
@@ -1,2 +1,2 @@
-
-::Tramway::Admin.set_available_models  ::Tramway::User::User, project: :purple_magic, role: :admin
+Tramway::Core.initialize_application model_class: PurpleMagic
+Tramway::Admin.set_singleton_models PurpleMagic, project: :purple_magic
