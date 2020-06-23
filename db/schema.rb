ab7bdfa300b983ee7913f922fdc73ed2218341df
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-06-24 00:11:28 +0400

    Fix bug with pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200507140813) do
+ActiveRecord::Schema.define(version: 20200623200511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20200507140813) do
     t.text "favicon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "main_image"
   end
 
   create_table "tramway_conference_unities", force: :cascade do |t|
