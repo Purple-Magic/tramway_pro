b191a274e393fd1ea86642d3267c440f9962c316
moshinaan <moshinahacker@yandex.ru>
2020-08-10 14:36:14 +0400

    fix test Show institution

--- spec/features/admin/institutions/show_spec.rb
+++ spec/features/admin/institutions/show_spec.rb
@@ -7,12 +7,12 @@ describe 'Show institution' do
 
   it 'should show institution' do
     visit '/admin'
-    fill_in 'Email', with: 'admin@email.com'
+    fill_in 'Email', with: "admin#{it_way_id}@email.com"
     fill_in 'Пароль', with: '123456'
     click_on 'Войти', class: 'btn-success'
 
-    last_institution = Tramway::SportSchool::Institution.last
-    click_on 'Спортивная школа'
+    last_institution = Tramway::Conference::Unity.last
+    click_on 'Проект'
 
     expect(page).to have_content last_institution.title
   end
