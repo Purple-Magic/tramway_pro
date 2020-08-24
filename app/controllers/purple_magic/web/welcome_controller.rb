2e1306aff7ed59880aae8360e02538047bce4f06
moshinaan <moshinahacker@yandex.ru>
2020-08-24 19:59:30 +0400

    rubocop at works

--- app/controllers/purple_magic/web/welcome_controller.rb
+++ app/controllers/purple_magic/web/welcome_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PurpleMagic::Web::WelcomeController < ApplicationController
   before_action :application
 
--- config/initializers/tramway/purple_magic.rb
+++ config/initializers/tramway/purple_magic.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Tramway::Admin.set_available_models(
   Tramway::Landing::Block,
   Tramway::Page::Page,
