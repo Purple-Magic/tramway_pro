9a5036777e862ca7cb640cd27ff0e12e6864fb9d
moshinaan <moshinahacker@yandex.ru>
2020-08-24 19:48:12 +0400

    rubocop at works

--- app/controllers/purple_magic/web/welcome_controller.rb
+++ app/controllers/purple_magic/web/welcome_controller.rb
@@ -1,14 +1,14 @@
 class PurpleMagic::Web::WelcomeController < ApplicationController
   before_action :application
-  
+
   layout 'tramway/landing/application'
 
   def index
     @blocks = ::Tramway::Landing::BlockDecorator.decorate ::Tramway::Landing::Block.on_main_page
   end
-  
+
   private
-  
+
   def application
     @application = ::Tramway::Core.application_object
   end
