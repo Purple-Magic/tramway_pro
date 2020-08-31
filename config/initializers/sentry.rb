e5657bc499e232e809463131db2d70ab6605bf5d
moshinaan <moshinahacker@yandex.ru>
2020-08-31 23:58:46 +0400

    #56 fix sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Raven.configure do |config|
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.dsn = 'https://656c786758804eecb4fac556f825c0f5@o351882.ingest.sentry.io/5412466'
 end
