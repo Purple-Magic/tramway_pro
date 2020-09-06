9106a1d54b9a5a7c112f8b2dbad7799700ab5358
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-07 03:58:37 +0400

    #chat_quest_ulsk update message_form

--- app/forms/admin/chat_quest_ulsk/message_form.rb
+++ app/forms/admin/chat_quest_ulsk/message_form.rb
@@ -1,11 +1,12 @@
 class Admin::ChatQuestUlsk::MessageForm < Tramway::Core::ApplicationForm
-  properties :text, :area, :position, :project_id
+  properties :text, :area, :position, :project_id, :answers
 
   def initialize(obj)
     super(obj).tap do
       form_properties text: :text,
         area: :default,
-        position: :default
+        position: :default,
+        answers: :default
     end
   end
 end
