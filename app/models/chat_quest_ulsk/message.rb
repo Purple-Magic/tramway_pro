7e7c217ce4e2fa3fbae10ccfc5cc0915879a3cf4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-07 06:08:08 +0400

    #chat_quest_ulsk update uploader for messages

--- app/models/chat_quest_ulsk/message.rb
+++ app/models/chat_quest_ulsk/message.rb
@@ -1,5 +1,5 @@
 class ChatQuestUlsk::Message < ApplicationRecord
   enumerize :area, in: ['Ленинский', 'Засвияжский', 'Заволжский', 'Железнодорожный']
 
-  uploader :file, :file
+  uploader :file, :file, extensions: [ :mp3, :wav ]
 end
