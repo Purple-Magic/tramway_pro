dd69fbedefc01c380af7025fda39ba5cec5ef5c6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-07 06:39:51 +0400

    Update deploy

--- lib/tasks/chatquestsulsk/after_svyaga_area_quest.rb
+++ lib/tasks/chatquestsulsk/after_svyaga_area_quest.rb
@@ -24,6 +24,10 @@ module ChatQuestUlsk::AfterSvyagaAreaQuest
         game.update! current_position: game.current_position + 1
         next_message = ChatQuestUlsk::Message.where(area: game.area, position: game.current_position).first
         bot.api.send_message chat_id: message.chat.id, text: next_message.text
+      elsif expecting_answers(game)&.include? message.text
+        game.update! current_position: game.current_position + 1
+        next_message = ChatQuestUlsk::Message.where(area: game.area, position: game.current_position).first
+        bot.api.send_message chat_id: message.chat.id, text: next_message.text
       end
     end
   end
