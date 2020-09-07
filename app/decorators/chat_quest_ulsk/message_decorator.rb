8cd104ebfd1ad94668aba8a7f23374a0e45afd28
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-07 04:01:04 +0400

    #chat_quest_ulsk update message_decorator

--- app/decorators/chat_quest_ulsk/message_decorator.rb
+++ app/decorators/chat_quest_ulsk/message_decorator.rb
@@ -1,6 +1,6 @@
 class ChatQuestUlsk::MessageDecorator < Tramway::Core::ApplicationDecorator
   def title
-    "#{object.area}: #{object.text.first(15)}..."
+    "#{object.area}: #{object.text.split(' ')[0..2].join(' ')}..."
   end
 
   delegate_attributes :area
