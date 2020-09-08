94c4cd5818b988e23eebab0bffa71eaa59feebdd
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-08 12:50:58 +0400

    #chat_quest_ulsk add more genres

--- app/models/chat_quest_ulsk/message.rb
+++ app/models/chat_quest_ulsk/message.rb
@@ -1,5 +1,5 @@
 class ChatQuestUlsk::Message < ApplicationRecord
-  enumerize :quest, in: [ :love ]
+  enumerize :quest, in: [ :love, :detective, :horror, :fantasy ]
 
   uploader :file, :file, extensions: [ :mp3, :wav, :jpg, :jpeg, :png ]
 end
