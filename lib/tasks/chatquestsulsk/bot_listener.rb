f100f0f8019b83f279f57ca3605ae66429c793b4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-08 16:56:29 +0400

    #chatquestsulsk separate bots

--- lib/tasks/chatquestsulsk/bot_listener.rb
+++ lib/tasks/chatquestsulsk/bot_listener.rb
@@ -7,25 +7,28 @@ require_relative './bot_answers'
 require_relative './love'
 require_relative './detective'
 
-include ChatQuestUlsk::BotInfo
-include ChatQuestUlsk::BotMessage
-include ChatQuestUlsk::BotAnswers
 
-quests = [ :love, :detective ]
+module BotListener
+  class << self
+    include ChatQuestUlsk::BotInfo
+    include ChatQuestUlsk::BotMessage
+    include ChatQuestUlsk::BotAnswers
 
-quests.each do |quest|
-  Telegram::Bot::Client.run(ENV["QUEST_ULSK_#{quest.upcase}_TELEGRAM_API_TOKEN"]) do |bot|
-    bot.listen do |message|
-      user = user_from message
-      chat = chat_from message
-      log_message message, user, chat
-      game = ChatQuestUlsk::Game.where(game_state: :started, quest: :love).find_by bot_telegram_user_id: user.id
-      if message.text == '/start'
-        unless game.present?
-          game = ChatQuestUlsk::Game.create! bot_telegram_user_id: user.id, quest: :love, current_position: 1 
+    def run_bot(quest:)
+      Telegram::Bot::Client.run(ENV["QUEST_ULSK_#{quest.upcase}_TELEGRAM_API_TOKEN"]) do |bot|
+        bot.listen do |message|
+          user = user_from message
+          chat = chat_from message
+          log_message message, user, chat
+          game = ChatQuestUlsk::Game.where(game_state: :started, quest: :love).find_by bot_telegram_user_id: user.id
+          if message.text == '/start'
+            unless game.present?
+              game = ChatQuestUlsk::Game.create! bot_telegram_user_id: user.id, quest: :love, current_position: 1 
+            end
+          end
+          "ChatQuestUlsk::#{quest.capitalize}".constantize.scenario(message, game, user, bot)
         end
       end
-      "ChatQuestUlsk::#{quest.capitalize}".constantize.scenario(message, game, user, bot)
     end
   end
 end
