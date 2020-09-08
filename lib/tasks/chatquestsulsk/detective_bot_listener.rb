0cb29774c62e168a9ed6e3745943d2c1dc8e184f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-08 16:56:29 +0400

    #chatquestsulsk separate bots

---
+++ lib/tasks/chatquestsulsk/detective_bot_listener.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'telegram/bot'
+require_relative './bot_info'
+require_relative './bot_message'
+require_relative './bot_answers'
+require_relative './love'
+require_relative './detective'
+
+include ChatQuestUlsk::BotInfo
+include ChatQuestUlsk::BotMessage
+include ChatQuestUlsk::BotAnswers
+
+Telegram::Bot::Client.run(ENV["QUEST_ULSK_DETECTIVE_TELEGRAM_API_TOKEN"]) do |bot|
+  bot.listen do |message|
+    user = user_from message
+    chat = chat_from message
+    log_message message, user, chat
+    game = ChatQuestUlsk::Game.where(game_state: :started, quest: :love).find_by bot_telegram_user_id: user.id
+    if message.text == '/start'
+      unless game.present?
+        game = ChatQuestUlsk::Game.create! bot_telegram_user_id: user.id, quest: :love, current_position: 1 
+      end
+    end
+    ChatQuestUlsk::Detective.scenario(message, game, user, bot)
+  end
+end
