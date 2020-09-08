4a0347e3146c5fdd66e89fd92c022c286e73898d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-08 13:26:09 +0400

    #chatquestsulsk update quest love

--- lib/tasks/chatquestsulsk/bot_answers.rb
+++ lib/tasks/chatquestsulsk/bot_answers.rb
@@ -1,7 +1,7 @@
 module ChatQuestUlsk::BotAnswers
   def expecting_answers(game)
     if game&.current_position.present?
-      ChatQuestUlsk::Message.find_by(area: game.area, position: game.current_position).answer
+      ChatQuestUlsk::Message.find_by(quest: game.quest, position: game.current_position).answer
     end
   end
 end
