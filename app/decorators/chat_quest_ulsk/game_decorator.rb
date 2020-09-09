c5c8cb357da5f68381b579ec8ad0ebbb1e4469b9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-09 04:43:48 +0400

    #chatquestsulsk upgrade admin

--- app/decorators/chat_quest_ulsk/game_decorator.rb
+++ app/decorators/chat_quest_ulsk/game_decorator.rb
@@ -1,7 +1,47 @@
 class ChatQuestUlsk::GameDecorator < Tramway::Core::ApplicationDecorator
+  class << self
+    delegate :human_game_state_event_name, to: :model_class
+
+    def show_attributes
+      [ :chat ]
+    end
+  end
+
   def title
     "#{object.quest}: #{user.title} ##{object.id}"
   end
 
   decorate_associations :user
+
+  def chat
+    content_tag :table do
+      object.messages.each do |message|
+        concat(content_tag(:tr) do
+          concat(content_tag(:td) do
+            case message.class.to_s
+            when 'ChatQuestUlsk::Message'
+              'Bot'
+            when 'BotTelegram::Message'
+              message.user.username
+            end
+          end)
+          concat(content_tag(:td) do
+            case message.class.to_s
+            when 'ChatQuestUlsk::Message'
+              message.text.present? ? message.text : '[файл]'
+            when 'BotTelegram::Message'
+              message.user.username
+            end
+          end)
+        end)
+      end
+    end
+  end
+
+  def game_state_button_color(event)
+    case event
+    when :finish
+      :success
+    end
+  end
 end
--- app/models/chat_quest_ulsk/game.rb
+++ app/models/chat_quest_ulsk/game.rb
@@ -1,4 +1,31 @@
 class ChatQuestUlsk::Game < ApplicationRecord
+  belongs_to :user, class_name: 'BotTelegram::User', foreign_key: :bot_telegram_user_id
+
+  def finished_at
+    audits.find_by(action: :update, audited_changes: { 'game_state' => [ 'started', 'finished' ] })&.created_at
+  end
+
+  def messages
+    user_messages = user.messages.where('created_at > ? AND created_at < ?', created_at, finished_at || DateTime.now)
+    events = audits.map do |audit|
+      audit if audit.audited_changes['current_position'].present?
+    end.compact
+    sorted_collection = (events + user_messages).sort_by { |obj| obj&.created_at }
+    sorted_collection.map do |obj|
+      case obj.class.to_s
+      when 'Audited::Audit'
+        case obj.action
+        when 'create'
+          ChatQuestUlsk::Message.find_by(quest: quest, position: 1)
+        when 'update'
+          ChatQuestUlsk::Message.find_by(quest: quest, position: obj.audited_changes['current_position'][1])
+        end
+      when 'BotTelegram::Message'
+        obj
+      end
+    end.compact
+  end
+
   state_machine :game_state, initial: :started do
     state :started
     state :finished
