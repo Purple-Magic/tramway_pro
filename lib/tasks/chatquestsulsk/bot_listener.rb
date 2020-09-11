b660894e77d8893120f15cb027ecfdb902c5e715
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-11 20:15:36 +0400

    Fix

--- lib/tasks/chatquestsulsk/bot_listener.rb
+++ lib/tasks/chatquestsulsk/bot_listener.rb
@@ -22,7 +22,7 @@ module BotListener
           user = user_from message
           chat = chat_from message
           log_message message, user, chat
-          game = ChatQuestUlsk::Game.active.where(game_state: :started, quest: quest).find_by bot_telegram_user_id: user.id
+          game = ChatQuestUlsk::Game.active.where(game_state: :started, quest: quest, bot_telegram_user_id: user.id).last
           if message.text == '/start'
             unless game.present?
               game = ChatQuestUlsk::Game.create! bot_telegram_user_id: user.id, quest: quest, current_position: 1, project_id: Project.find_by(title: 'PurpleMagic').id
