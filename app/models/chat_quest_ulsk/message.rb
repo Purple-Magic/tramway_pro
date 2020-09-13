90bc6546efc0d80abaa7f925f159317330a037ac
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-13 18:59:19 +0400

    #chat_quest_ulsk add search for scenarios

--- app/models/chat_quest_ulsk/message.rb
+++ app/models/chat_quest_ulsk/message.rb
@@ -2,4 +2,6 @@ class ChatQuestUlsk::Message < ApplicationRecord
   enumerize :quest, in: %i[love detective horror fantasy]
 
   uploader :file, :file, extensions: %i[mp3 wav jpg jpeg png]
+
+  search_by :text, :quest, :answer
 end
