9f1d438b8915ce9197d096f9391d89022d0bfe12
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-10-02 17:42:14 +0400

    Export participant by age

--- bin/export_it_way_participants_by_age.rb
+++ bin/export_it_way_participants_by_age.rb
@@ -1,18 +1,23 @@
 require 'csv'
 
+age = 16
+
 CSV.open('list.csv', 'w') do |csv|
   participants = Tramway::Event::Participant.where project_id: 2
   participants.each do |participant|
     if participant.values.present?
       begin
         birth_date = participant.values['Дата рождения']&.to_datetime
-        if birth_date.present? && (DateTime.new(2020, 10, 23) - birth_date) < 16.years
-          email = participant.values['Email']
-          if email.present?
-          csv << [
-            "#{participant.values['Фамилия']} #{participant.values['Имя']} #{participant.values['Отчество']}",
-            email
-          ]
+        if birth_date.present?
+          difference = (Time.new(2020, 10, 23) - birth_date.to_time) / 3600 / 24 / 365
+          if difference > age
+            email = participant.values['Email']
+            if email.present?
+              csv << [
+                "#{participant.values['Фамилия']} #{participant.values['Имя']} #{participant.values['Отчество']}",
+                email
+              ]
+            end
           end
         end
       rescue StandardError => e
