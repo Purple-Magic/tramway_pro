4899d22d6bb8ec662dccc350e22e6c52c0f3fecb
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-10-11 08:02:48 +0400

    Show and update work for podcasts too

--- spec/features/admin/podcasts/update_spec.rb
+++ spec/features/admin/podcasts/update_spec.rb
@@ -2,44 +2,30 @@
 
 require 'rails_helper'
 
-describe 'Update event' do
+describe 'Update podcast' do
   before { move_host_to it_way_host }
-  let!(:attributes) { attributes_for :event_admin_attributes }
-  before { create :event, project_id: it_way_id }
+  let!(:attributes) { attributes_for :podcast_admin_attributes }
+  before { create :podcast, project_id: it_way_id }
 
-  it 'should update event' do
+  it 'should update podcast' do
     visit '/admin'
     fill_in 'Email', with: "admin#{it_way_id}@email.com"
     fill_in 'Пароль', with: '123456'
     click_on 'Войти', class: 'btn-success'
 
-    event = Tramway::Event::Event.last
-    click_on_dropdown 'Организация мероприятий'
-    click_on 'Мероприятия'
-    click_on event.title
+    podcast = Podcast.last
+    click_on 'Подкасты'
+    click_on podcast.title
     find('.btn.btn-warning', match: :first).click
     fill_in 'record[title]', with: attributes[:title]
-    fill_in 'record[begin_date]', with: attributes[:begin_date]
-    fill_in 'record[end_date]', with: attributes[:end_date]
-    fill_in 'record[request_collecting_begin_date]', with: attributes[:request_collecting_begin_date]
-    fill_in 'record[request_collecting_end_date]', with: attributes[:request_collecting_end_date]
-    select attributes[:reach], from: 'record[reach]'
+    fill_in 'record[feed_url]', with: attributes[:feed_url]
 
     click_on 'Сохранить', class: 'btn-success'
-    event.reload
+    podcast.reload
     attributes.keys.each do |attr|
-      actual = event.send(attr)
+      actual = podcast.send(attr)
       expecting = attributes[attr]
-      if attr.in? %i[begin_date end_date request_collecting_end_date request_collecting_begin_date]
-        actual = actual.to_datetime
-      end
-      case actual.class.to_s
-      when 'NilClass'
-        expect(actual).not_to be_empty, "#{attr} is empty"
-      when 'Enumerize::Value'
-        expect(actual).not_to be_empty, "#{attr} is empty"
-        actual = actual.text
-      end
+      expect(actual).not_to be_empty, "#{attr} is empty"
       expect(actual).to eq(expecting), problem_with(attr: attr, expecting: expecting, actual: actual)
     end
   end
