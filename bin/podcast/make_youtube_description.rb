ba9bc748775449cde9fc056986adcf329577b8b5
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-11-26 03:28:03 +0400

    #podcast added parser for podcasts

---
+++ bin/podcast/make_youtube_description.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+
+doc = File.open(Rails.root.join('bin', 'podcast', 'description.html')) do |f|
+  Nokogiri::HTML(f, nil, Encoding::UTF_8.to_s)
+end
+
+def recursively_build(elements)
+  elements.map do |element|
+    case element.name
+    when 'text', 'h1'
+      element.text
+    when 'a'
+      "#{element.text} #{element[:href]}"
+    when 'li'
+      "  * #{recursively_build(element.children)}"
+    else
+      recursively_build element.children
+    end
+  end.join("\n")
+end
+
+text = recursively_build doc.elements
+puts text
