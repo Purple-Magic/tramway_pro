ec4133ac5ddb63ec2a63d7d755f9c7cb5350d226
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-12-20 15:47:07 +0400

    BOTS updates file uploader

--- app/models/bot_telegram/scenario/step.rb
+++ app/models/bot_telegram/scenario/step.rb
@@ -4,7 +4,7 @@ class BotTelegram::Scenario::Step < ApplicationRecord
   belongs_to :bot
   has_many :progress_records, class_name: 'BotTelegram::Scenario::ProgressRecord', foreign_key: :bot_telegram_scenario_step_id
 
-  uploader :file, :file, extensions: %i[mp3 wav jpg jpeg png]
+  uploader :file, :file, extensions: %i[mp3 wav jpg jpeg png ogg]
 
   scope :partner_scope, -> (_user_id) { all }
   [ :rsm, :night ].each do |team|
