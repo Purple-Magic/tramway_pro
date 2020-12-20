7a1944e042a03738c94df3fde0dd3c36c43ff6f3
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-12-20 14:57:59 +0400

    BOTS updates start_bots script

--- lib/tasks/bot_telegram/start_bots.rb
+++ lib/tasks/bot_telegram/start_bots.rb
@@ -1,3 +1,6 @@
+require 'colorize'
+
 Bot.active.find_each do |bot|
   system "RUNNING_BOT_NAME=#{bot.name} /bin/bash -lic 'exec bundle exec rails runner lib/tasks/bot_telegram/bot_listener.rb &'"
+  puts "#{bot.name} started".green
 end
