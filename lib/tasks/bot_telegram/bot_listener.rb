b2383d4447a6c92c7f69b3007cb0f7b4b3fdc38b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-12-20 14:46:53 +0400

    BOTS fix

--- lib/tasks/bot_telegram/bot_listener.rb
+++ lib/tasks/bot_telegram/bot_listener.rb
@@ -4,25 +4,20 @@ require 'telegram/bot'
 require_relative 'scenario'
 
 module BotTelegram
-  module BotListener
-    class << self
-      include BotTelegram::Info
-      include BotTelegram::MessagesManager
+  class BotListener
+    include BotTelegram::Info
+    include BotTelegram::MessagesManager
 
-      def run_bot
-        Bot.active.find_each do |bot_record|
-          Telegram::Bot::Client.run(bot_record.token) do |bot|
-            bot.listen do |message|
-              user = user_from message
-              chat = chat_from message
-              log_message message, user, chat
-              BotTelegram::Scenario.run message, bot, bot_record
-            end
-          end
+    def perform
+      bot_record = Bot.find_by name: ENV['RUNNING_BOT_NAME']
+      Telegram::Bot::Client.run(bot_record.token) do |bot|
+        bot.listen do |message|
+          user = user_from message
+          chat = chat_from message
+          log_message message, user, chat
+          BotTelegram::Scenario.run message, bot, bot_record
         end
       end
     end
   end
 end
-
-BotTelegram::BotListener.run_bot
---
+++ lib/tasks/bot_telegram/start_bots.rb
@@ -0,0 +1,3 @@
+Bot.active.find_each do |bot|
+  system "RUNNING_BOT_NAME=#{bot.name} /bin/bash -lic 'exec bundle exec rails runner lib/tasks/bot_telegram/bot_listener.rb &'"
+end
