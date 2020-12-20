b279057e2dda4cf73610db7de8bf824616d8da74
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-12-20 15:19:24 +0400

    Fix

--- lib/tasks/bot_telegram/bot_listener.rb
+++ lib/tasks/bot_telegram/bot_listener.rb
@@ -5,19 +5,23 @@ require_relative 'scenario'
 
 module BotTelegram
   class BotListener
-    include BotTelegram::Info
-    include BotTelegram::MessagesManager
+    class << self
+      include BotTelegram::Info
+      include BotTelegram::MessagesManager
 
-    def perform
-      bot_record = Bot.find_by name: ENV['RUNNING_BOT_NAME']
-      Telegram::Bot::Client.run(bot_record.token) do |bot|
-        bot.listen do |message|
-          user = user_from message
-          chat = chat_from message
-          log_message message, user, chat
-          BotTelegram::Scenario.run message, bot, bot_record
+      def perform
+        bot_record = Bot.find_by name: ENV['RUNNING_BOT_NAME']
+        Telegram::Bot::Client.run(bot_record.token) do |bot|
+          bot.listen do |message|
+            user = user_from message
+            chat = chat_from message
+            log_message message, user, chat
+            BotTelegram::Scenario.run message, bot, bot_record
+          end
         end
       end
     end
   end
 end
+
+BotTelegram::BotListener.perform
--- lib/tasks/bot_telegram/start_bots.rb
+++ lib/tasks/bot_telegram/start_bots.rb
@@ -1,6 +1,8 @@
 require 'colorize'
 
 Bot.active.find_each do |bot|
-  system "RUNNING_BOT_NAME=\"#{bot.name}\" /bin/bash -lic 'exec bundle exec rails runner lib/tasks/bot_telegram/bot_listener.rb &'"
+  command = "RUNNING_BOT_NAME=\"#{bot.name}\" /bin/bash -lic 'exec bundle exec rails runner lib/tasks/bot_telegram/bot_listener.rb &'"
+  puts command.blue
+  system command
   puts "#{bot.name} started".green
 end
