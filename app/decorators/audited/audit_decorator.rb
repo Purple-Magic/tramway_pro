eae4f502dbdd178ef56c835aec11986aa37f8da0
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-12-24 01:14:12 +0400

    Updates

---
+++ app/decorators/audited/audit_decorator.rb
@@ -0,0 +1,59 @@
+class Audited::AuditDecorator < Tramway::Core::ApplicationDecorator
+  # Associations you want to show in admin dashboard
+  # decorate_associations :messages, :posts
+
+  class << self
+    def collections
+      # [ :all, :scope1, :scope2 ]
+      [ :all ]
+    end
+
+    def list_attributes
+      [
+        :id,
+        :username,
+        :action,
+        :audited_changes,
+      ]
+    end
+
+    def show_attributes
+      [
+        :id,
+        :username,
+        :action,
+        :audited_changes,
+        :version,
+        :comment,
+        :remote_address,
+        :request_uuid,
+        :created_at,
+      ]
+    end
+
+    def show_associations
+      # Associations you want to show in admin dashboard
+      # [ :messages ]
+    end
+
+    def list_filters
+      # {
+      #   filter_name: {
+      #     type: :select,
+      #     select_collection: filter_collection,
+      #     query: lambda do |list, value|
+      #       list.where some_attribute: value
+      #     end
+      #   },
+      #   date_filter_name: {
+      #     type: :dates,
+      #     query: lambda do |list, begin_date, end_date|
+      #       list.where 'created_at > ? AND created_at < ?', begin_date, end_date
+      #     end
+      #   }
+      # }
+    end
+  end
+
+  # delegate_attributes :title
+end
