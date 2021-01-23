173657546a3bcc0d624af378fc392899f6d24729
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-01-23 05:59:39 +0400

    Fix start bots

--- lib/tasks/bot_telegram/start_bots.rb
+++ lib/tasks/bot_telegram/start_bots.rb
@@ -1,8 +1,10 @@
 require 'colorize'
 
 Bot.active.find_each do |bot|
-  command = "RUNNING_BOT_NAME=\"#{bot.name}\" /bin/bash -lic 'exec bundle exec rails runner lib/tasks/bot_telegram/bot_listener.rb &'"
-  puts command.blue
-  system command
-  puts "#{bot.name} started".green
+  if bot.id != 9
+    command = "RUNNING_BOT_NAME=\"#{bot.name}\" /bin/bash -lic 'exec bundle exec rails runner lib/tasks/bot_telegram/bot_listener.rb &'"
+    puts command.blue
+    system command
+    puts "#{bot.name} started".green
+  end
 end
