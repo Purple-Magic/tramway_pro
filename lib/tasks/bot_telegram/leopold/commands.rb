a8ab433e662cc0c1643f6e39949291a2f8f82025
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-01-24 18:58:00 +0400

    Fix

--- lib/tasks/bot_telegram/leopold/commands.rb
+++ lib/tasks/bot_telegram/leopold/commands.rb
@@ -3,6 +3,8 @@ module BotTelegram
     module Commands
       def add_word(text)
         if text == '/add_word@myleopold_bot' || text == '/add_word'
+          message_from_telegram bot, chat, bot_record.options['empty_word']
+        else
           words = Word.find_records_by(text, Word.active)
           if words.any?
             message_to_chat bot, chat, bot_record.options['i_have_this_word']
@@ -10,8 +12,6 @@ module BotTelegram
             Word.create! main: text, review_state: :unviewed, project_id: 2
             message_to_chat bot, chat, bot_record.options['we_will_review_it']
           end
-        else
-          message_from_telegram bot, chat, bot_record.options['empty_word']
         end
       end
     end
