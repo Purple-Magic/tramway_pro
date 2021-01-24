88a69ff366c78d1c1cc823ede4a263b2f61ad0a6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-01-24 18:48:20 +0400

    Remove message

--- lib/tasks/bot_telegram/leopold/commands.rb
+++ lib/tasks/bot_telegram/leopold/commands.rb
@@ -5,7 +5,6 @@ module BotTelegram
         words = Word.find_records_by(text, Word.active)
         if words.any?
           message_to_chat bot, chat, bot_record.options['i_have_this_word']
-          message_to_chat bot, chat, build_message_with_word(words.first)
         else
           Word.create! main: text, review_state: :unviewed, project_id: 2
           message_to_chat bot, chat, bot_record.options['we_will_review_it']
