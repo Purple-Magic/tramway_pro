6aff0e75e3041827fff9986a38e2e2b43a3fc18b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-01-24 18:09:54 +0400

    Fix

--- lib/tasks/bot_telegram/leopold/commands.rb
+++ lib/tasks/bot_telegram/leopold/commands.rb
@@ -2,9 +2,10 @@ module BotTelegram
   module Leopold
     module Commands
       def add_word(text)
-        if Word.find_records_by(text, Word.active).any?
+        words = Word.find_records_by(text, Word.active)
+        if words.any?
           message_to_chat bot, chat, bot_record.options['i_have_this_word']
-          message_to_chat bot, chat, build_message_with_word(word)
+          message_to_chat bot, chat, build_message_with_word(word.first)
         else
           Word.create! main: text, review_state: :unviewed
           message_to_chat bot, chat, bot_record.options['we_will_review_it']
