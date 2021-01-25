b59e5fc638b1fc2b3faa48e8c2ae1a1d9343056f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-01-25 16:31:05 +0400

    Fix

--- lib/tasks/bot_telegram/leopold/it_way_pro/words_check.rb
+++ lib/tasks/bot_telegram/leopold/it_way_pro/words_check.rb
@@ -3,13 +3,17 @@ module BotTelegram
     module ItWayPro
       module WordsCheck
         def words_to_explain(text)
-          ['.', ',', '!', ':', ';', '(', ')', '@'].each do |symbol|
-            text.gsub! symbol, ''
+          if text.present?
+            ['.', ',', '!', ':', ';', '(', ')', '@'].each do |symbol|
+              text.gsub! symbol, ''
+            end
+            words = text.split(' ')
+            words.map do |word|
+              Word.find_records_by word, Word.active.approved
+            end.flatten.uniq
+          else
+            []
           end
-          words = text.split(' ')
-          words.map do |word|
-            Word.find_records_by word, Word.active.approved
-          end.flatten.uniq
         end
       end
     end
