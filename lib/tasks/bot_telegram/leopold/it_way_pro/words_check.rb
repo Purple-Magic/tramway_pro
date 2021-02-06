ea2ed3d0d9049eb24d5e327412ea248813a5b4b7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-02-07 00:12:52 +0400

    Fix words_check for BOT LEOPOLD

--- lib/tasks/bot_telegram/leopold/it_way_pro/words_check.rb
+++ lib/tasks/bot_telegram/leopold/it_way_pro/words_check.rb
@@ -3,14 +3,16 @@ module BotTelegram
     module ItWayPro
       module WordsCheck
         def words_to_explain(text)
-          Word.all.map do |word|
-            include_main = text.include?(word.main)
-            include_synonim = word.synonims.map do |synonim|
-              text.include? synonim
-            end.includes? true
+          if text.present?
+            Word.all.map do |word|
+              include_main = text.include?(word.main)
+              include_synonim = word.synonims.map do |synonim|
+                text.include? synonim
+              end.includes? true
 
-            return word if include_main || include_synonim
-          end.compact
+              return word if include_main || include_synonim
+            end.compact
+          end
         end
       end
     end
