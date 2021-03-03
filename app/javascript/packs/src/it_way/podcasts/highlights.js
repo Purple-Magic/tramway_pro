5a37d0af891499bf0ef56c1fb89e9280735a573c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-03 07:22:59 +0400

    Upgrade time in seconds

--- app/javascript/packs/src/it_way/podcasts/highlights.js
+++ app/javascript/packs/src/it_way/podcasts/highlights.js
@@ -11,14 +11,11 @@ document.addEventListener("DOMContentLoaded", function() {
     let diffInSec = (diffInMin - mm) * 60;
     let ss = Math.floor(diffInSec);
 
-    let diffInMs = (diffInSec - ss) * 100;
-    let ms = Math.floor(diffInMs);
-
+    let formattedHH = hh.toString().padStart(2, "0");
     let formattedMM = mm.toString().padStart(2, "0");
     let formattedSS = ss.toString().padStart(2, "0");
-    let formattedMS = ms.toString().padStart(2, "0");
 
-    return `${formattedMM}:${formattedSS}:${formattedMS}`;
+    return `${formattedHH}:${formattedMM}:${formattedSS}`;
   }
 
   let startTime;
