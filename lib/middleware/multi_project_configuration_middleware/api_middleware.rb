a6128a87cc0cda79b89120bdeb4ffbbb0797dde1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-08 01:22:51 +0400

    FIx middleware

--- lib/middleware/multi_project_configuration_middleware/api_middleware.rb
+++ lib/middleware/multi_project_configuration_middleware/api_middleware.rb
@@ -42,6 +42,38 @@ module MultiProjectCallbacks
 
           params[:data][:attributes][:project_id] = Project.where(url: ENV['PROJECT_URL']).first.id
         end
+
+        before_action :load_application
+
+        def load_application
+          if engine_loaded(request).present?
+            build_application_with_engine engine_loaded request
+          elsif application_class(request).present?
+            @application = application_class(request).camelize.constantize.first
+          else
+            @application = application_object request
+          end
+        end
+
+        private
+
+        def build_application_with_engine(engine_loaded)
+          engine_module = "::Tramway::#{engine_loaded.camelize}".constantize
+          @application = "#{engine_module}::#{engine_module.application.to_s.camelize}".constantize.first
+          @application_engine = engine_loaded
+        end
+
+        def application_class(request)
+          Constraints::DomainConstraint.new(request.domain).application_class
+        end
+
+        def engine_loaded(request)
+          Constraints::DomainConstraint.new(request.domain).engine_loaded
+        end
+
+        def application_object(request)
+          Constraints::DomainConstraint.new(request.domain).application_object
+        end
       end
     end
 
