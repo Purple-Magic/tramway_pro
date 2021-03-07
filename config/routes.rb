c9a08880b2bda87ad19df59cc9e5574ad82dbad3
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-08 02:24:26 +0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   constraints Constraints::DomainConstraint.new(Settings[Rails.env][:it_way]) do
-    mount Tramway::Conference::Engine => '/'
+    mount Tramway::Conference::Engine => '/', as: :it_way
     mount Tramway::Api::Engine, at: '/api', as: :it_way_api
     scope module: :it_way do
       resources :certificates, only: :show
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   end
 
   constraints Constraints::DomainConstraint.new(Settings[Rails.env][:kalashnikovisme]) do
-    root to: 'kalashnikovisme/web/welcome#index'
+    root to: 'kalashnikovisme/web/welcome#index', as: :kalashnikovisme
 
     mount Tramway::Admin::Engine, at: '/admin', as: :kalash_admin
     mount Tramway::Auth::Engine, at: '/auth', as: :kalash_auth
@@ -19,35 +19,35 @@ Rails.application.routes.draw do
   end
 
   constraints Constraints::DomainConstraint.new(Settings[Rails.env][:tramway_dev]) do
-    root to: 'tramway_dev/web/welcome#index'
+    root to: 'tramway_dev/web/welcome#index', as: :tramway_dev
 
     mount Tramway::Admin::Engine, at: '/admin'
     mount Tramway::Auth::Engine, at: '/auth'
   end
 
   constraints Constraints::DomainConstraint.new(Settings[Rails.env][:engineervol]) do
-    root to: 'engineervol/web/welcome#index'
+    root to: 'engineervol/web/welcome#index', as: :engineervol
 
     mount Tramway::Admin::Engine, at: '/admin', as: :vol_admin
     mount Tramway::Auth::Engine, at: '/auth', as: :vol_auth
   end
 
   constraints Constraints::DomainConstraint.new(Settings[Rails.env][:purple_magic]) do
-    root to: 'purple_magic/web/welcome#index'
+    root to: 'purple_magic/web/welcome#index', as: :purple_magic
 
     mount Tramway::Admin::Engine, at: '/admin', as: :purple_magic_admin
     mount Tramway::Auth::Engine, at: '/auth', as: :purple_magic_auth
   end
 
   constraints Constraints::DomainConstraint.new(Settings[Rails.env][:gorodsad73]) do
-    root to: 'gorodsad73/web/welcome#index'
+    root to: 'gorodsad73/web/welcome#index', as: :gorod
 
     mount Tramway::Admin::Engine, at: '/admin', as: :gorod_admin
     mount Tramway::Page::Engine, at: '/page', as: :gorod_page
   end
 
   constraints Constraints::DomainConstraint.new(Settings[Rails.env][:red_magic]) do
-    root to: 'red_magic/web/welcome#index'
+    root to: 'red_magic/web/welcome#index', as: :red_magic_root
 
     mount Tramway::Admin::Engine, at: '/admin', as: :red_magic_admin
     mount Tramway::Auth::Engine, at: '/auth', as: :red_magic_auth
