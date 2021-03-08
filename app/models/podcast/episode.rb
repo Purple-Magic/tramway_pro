c610cf9a9ed5a0517a7291b3923d6920c7fc5c40
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-09 00:04:30 +0400

    Add cutting files

--- app/models/podcast/episode.rb
+++ app/models/podcast/episode.rb
@@ -20,5 +20,18 @@ class Podcast::Episode < ApplicationRecord
   end
 
   def cut_highlights
+    filename = file.path.split('.')[0..-2].join('.')
+    if file.path.split('.').last == 'ogg'
+      filename = filename + '.mp3'
+      system "ffmpeg -i #{file.path} #{filename}"
+    end
+    highlights.each do |highlight|
+      hour = highlight.time.split(':')[0]
+      minutes = highlight.time.split(':')[1]
+      seconds = highlight.time.split(':')[2]
+      begin_time = (DateTime.new(2020, 01, 01, hour.to_i, minutes.to_i, seconds.to_i) - 2.minutes).strftime '%H:%M:%S'
+      end_time = highlight.time
+      system "ffmpeg -i #{filename} -ss #{begin_time} -to #{end_time} -c copy #{Rails.root}/public/#{podcast.title}-#{episode.number}-part-#{index + 1}.mp3"
+    end
   end
 end
