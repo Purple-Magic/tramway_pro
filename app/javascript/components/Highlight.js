6a5a63a16b9d74465ebf30973033dc6e5eb2c30a
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-08 12:39:51 +0400

    Upgrade podcasts highlights page

--- app/javascript/components/Highlight.js
+++ app/javascript/components/Highlight.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { index, show, create } from '../packs/src/crud'
 import { Button, Form, ListGroup } from 'react-bootstrap'
+import _ from 'underscore'
 
 const podcastOptions = (podcasts) => {
   return podcasts.map((podcast) => {
@@ -12,9 +13,9 @@ const podcastOptions = (podcasts) => {
 const highlightsList = (highlights) => {
   return highlights.map((highlight) => {
     return (
-      <li key={highlight.id}>
+      <ListGroup.Item key={highlight.id}>
         {highlight.attributes.time}
-      </li>
+      </ListGroup.Item>
     )
   })
 }
@@ -34,7 +35,7 @@ class Highlight extends React.Component {
 
     setInterval(() => {
       if (this.state.episode) {
-        show('Podcast::Episode', this.state.episode.id).then((response) => {
+        show('Podcast::Episode', this.state.episode.data.id).then((response) => {
           const included = response.data.included
           if (included) {
             this.setState({
@@ -50,13 +51,16 @@ class Highlight extends React.Component {
     index('Podcast').then((response) => {
       this.setState({
         podcasts: response.data.data,
+        params: {
+          podcast_id: _.first(response.data.data).id
+        }
       })
     })
   }
 
   componentDidUpdate() {
     if (this.state.episode) {
-      document.getElementById('episodeId').value = this.state.episode.id
+      document.getElementById('episodeId').value = this.state.episode.data.id
     }
   }
 
@@ -72,7 +76,7 @@ class Highlight extends React.Component {
   createEpisode() {
     create('Podcast::Episode', this.state.params).then((response) => {
       this.setState({
-        episode: response.data.data,
+        episode: response.data,
       })
     })
   }
@@ -80,9 +84,14 @@ class Highlight extends React.Component {
   render () {
     if (this.state.episode) {
       return (
-        <ListGroup>
-          { highlightsList(this.state.highlights) }
-        </ListGroup>
+        <>
+          <h2>
+            { _.first(this.state.episode.included.filter(item => item.type == 'podcasts')).attributes.title }. Выпуск №{ this.state.episode.data.attributes.number }
+          </h2>
+          <ListGroup>
+            { highlightsList(this.state.highlights) }
+          </ListGroup>
+        </>
       );
     } else {
       return (
