11a689f4f9a2ca5e95bdde03ccceefb811d15917
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-23 01:30:12 +0400

    GORODSAD update main page

--- app/javascript/packs/src/gorodsad73/application.js
+++ app/javascript/packs/src/gorodsad73/application.js
@@ -2,7 +2,7 @@ import { jarallax, jarallaxElement } from 'jarallax'
 
 document.addEventListener("DOMContentLoaded", () => {
   jarallax(document.querySelectorAll('.jarallax'), {
-    speed: 0.2
+    speed: 0.5
   });
   jarallaxElement();
   jarallax(document.querySelectorAll("[data-jarallax-element]"));
