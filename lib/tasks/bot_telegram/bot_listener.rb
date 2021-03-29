1c997d1fef6165373fa74746d87a6731387df0eb
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-30 03:19:53 +0400

    Fix bot record message error

--- lib/tasks/bot_telegram/bot_listener.rb
+++ lib/tasks/bot_telegram/bot_listener.rb
@@ -26,7 +26,7 @@ class BotTelegram::BotListener
               BotTelegram::Scenario.run message, bot, bot_record
             end
           else
-            error = "Empty message in bot #{bot.id}"
+            error = "Empty message in bot #{bot_record.id}"
             Rails.env.development? ? puts(error) : Raven.capture_exception(error)
           end
         end
