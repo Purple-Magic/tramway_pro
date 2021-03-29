47f4c67e851c80cd82a9a808ed815db6989df35b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-30 01:49:54 +0400

    add sidekiq workers

--- app/jobs/podcast/highlight_job.rb
+++
@@ -1,6 +0,0 @@
-class Podcast::HighlightJob < ApplicationJob
-  def perform(id)
-    episode = Podcast::Episode.find id
-    episode.cut_highlights
-  end
-end
---
+++ app/workers/podcast/highlights_worker.rb
@@ -0,0 +1,7 @@
+class Podcast::HighlightsWorker
+  include Sidekiq::Worker
+
+  def perform(*args)
+    # Do something
+  end
+end
