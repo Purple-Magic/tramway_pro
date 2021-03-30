4c4d017af26e815edfcd9e00ff1482696b6bcddf
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-30 04:31:50 +0400

    Fix workers

--- app/controllers/red_magic/api/v1/podcast/highlights_controller.rb
+++ app/controllers/red_magic/api/v1/podcast/highlights_controller.rb
@@ -1,6 +1,6 @@
 class RedMagic::Api::V1::Podcast::HighlightsController < RedMagic::Api::V1::ApplicationController
   def create
-    ::Podcast::HighlightsWorker.perform_async params[:id]
+    ::Podcasts::HighlightsWorker.perform_async params[:id]
 
     redirect_to ::Tramway::Admin::Engine.routes.url_helpers.record_path(params[:id], model: Podcast::Episode)
   end
