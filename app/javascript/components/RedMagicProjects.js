3c07e23c37d8759ac4051133ae58b27b339705d9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-04-02 05:34:39 +0400

    Red Magic project page on react

--- app/javascript/components/RedMagicProjects.js
+++ app/javascript/components/RedMagicProjects.js
@@ -1,62 +1,74 @@
 import React from 'react'
+import { snakeCase } from "snake-case"
 
 class Projects extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <>
-      <div className="row-projects">
-      <div className="project red_magic_live">
-      <div className="cover red_magic_live">
-      Red Magic Live
-      </div>
-      </div>
-      <div className="project who_knew">
-      <div className="cover who_knew">
-      Хунью
-      </div>
-      </div>
-      </div>
-      <div className="row-projects">
-      <div className="project it_way">
-      <div className="cover it_way">
-      IT Way
-      </div>
-      </div>
-      <div className="project red_magic_tv">
-      <div className="cover red_magic_tv">
-      Red Magic TV
-      </div>
-      </div>
-      </div>
-        <div className="project-page active it_way_page">
-      <div className="red_magic_live">
-      <h1>
-      Red Magic Live
-      </h1>
-      </div>
-      <div className="it_way">
-      <h1>
-      IT Way
-      </h1>
-      <h2>
-      ИТ-сообщество
-      </h2>
-      <div>
-      </div>
-
-      </div>
-      <div className="red_magic_tv">
-      <h1>
-      Red Magic TV
-      </h1>
-      </div>
-      <div className="who_knew">
-      <h1>
-      Хунью
-      </h1>
-      </div>
-      </div>
-        </>
+        <div className="row-projects">
+          {
+            [ 'Red Magic Live', 'Who knew' ].map((project) => {
+              const snakeCaseName = snakeCase(project)
+              return (
+                <div className={`project ${snakeCaseName}`}>
+                  <div className={`cover ${snakeCaseName}`}>
+                    {project}
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="row-projects">
+          {
+            [ 'IT Way', 'Red Magic TV' ].map((project) => {
+              const snakeCaseName = snakeCase(project)
+              return (
+                <div className={`project ${snakeCaseName}`}>
+                  <div className={`cover ${snakeCaseName}`}>
+                    {project}
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="project-page it_way_page">
+          <div className="red_magic_live">
+            <h1>
+              Red Magic Live
+            </h1>
+          </div>
+          <div className="it_way">
+            <h1>
+              IT Way
+            </h1>
+            <h2>
+              ИТ-сообщество
+            </h2>
+            <div>
+            </div>
+          </div>
+          <div className="red_magic_tv">
+            <h1>
+              Red Magic TV
+            </h1>
+          </div>
+          <div className="who_knew">
+            <h1>
+              Хунью
+            </h1>
+          </div>
+        </div>
+      </>
     )
   }
 }
