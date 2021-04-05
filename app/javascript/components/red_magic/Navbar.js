5fc52b928d2aa26d2f32fed3d25b11dac74b70b2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-04-05 04:39:34 +0400

    RED MAGIC working react navbar

---
+++ app/javascript/components/red_magic/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react"
+import PropTypes from "prop-types"
+import LinkIcon from '../LinkIcon'
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showState: 'hidden'
+    }
+
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle() {
+    this.setState({
+      showState: this.state.showState === 'hidden' ? 'show' : 'hidden',
+    })
+  }
+
+  render () {
+    return (
+      <>
+        <nav className={this.state.showState}>
+          <ul>
+            {
+              this.props.links.map((link) => {
+                if (this.state.showState === 'show') {
+                  return (
+                    <li>
+                      <a href={link.url}>{link.text}</a>
+                    </li>
+                  )
+                }
+                return <></>
+              })
+            }
+          </ul>
+        </nav>
+        <div className={`toggle-button ${this.state.showState}`}>
+          <LinkIcon name={this.state.showState === 'hidden' ? 'bars' : 'times' } type="solid" onClick={() => { this.toggle() } } />
+        </div>
+      </>
+    );
+  }
+}
+
+export default Navbar
