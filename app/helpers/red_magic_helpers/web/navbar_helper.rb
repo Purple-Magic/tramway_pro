a302f467675ff6ad23cb8927aab0933fcf2079f2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-04-05 05:00:23 +0400

    Rubocop fixes

--- app/helpers/red_magic_helpers/web/navbar_helper.rb
+++ app/helpers/red_magic_helpers/web/navbar_helper.rb
@@ -2,7 +2,7 @@ module RedMagicHelpers
   module Web
     module NavbarHelper
       def links
-        [ { url: '#', text: 'Home' }, { url: '#', text: 'About' }, { url: '#', text: 'Service' }, { url: '#', text: 'Portfolio' }, { url: '#', text: 'Team' } ]
+        [{ url: '#', text: 'Home' }, { url: '#', text: 'About' }, { url: '#', text: 'Service' }, { url: '#', text: 'Portfolio' }, { url: '#', text: 'Team' }]
       end
     end
   end
