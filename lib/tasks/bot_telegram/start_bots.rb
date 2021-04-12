8be1af2c45a6b9827568e24d03b26ae957fe7789
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-04-13 00:39:43 +0400

    BOT LEOPOLD add logic to work with stickers

--- lib/tasks/bot_telegram/start_bots.rb
+++ lib/tasks/bot_telegram/start_bots.rb
@@ -4,7 +4,7 @@ require 'colorize'
 
 Bot.active.find_each do |bot|
   bot_listener_file = 'lib/tasks/bot_telegram/bot_listener.rb'
-  command = "RUNNING_BOT_NAME=\"#{bot.name}\" /bin/bash -lic 'exec bundle exec rails runner #{bot_listener_file} &'"
+  command = "RUNNING_BOT_ID=\"#{bot.id}\" /bin/bash -lic 'exec bundle exec rails runner #{bot_listener_file} &'"
   puts command.blue
   system command
   puts "#{bot.name} started".green
