89b9c3e43bc71b795df40c217d349eff83507efb
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-05-07 18:51:11 +0400

    RED MAGIC update serialized data

--- app/serializers/podcast/episode_serializer.rb
+++ app/serializers/podcast/episode_serializer.rb
@@ -3,6 +3,10 @@
 class Podcast::EpisodeSerializer < Tramway::Api::V1::ApplicationSerializer
   attributes :podcast_id, :number, :title, :number, :season, :description, :published_at, :image, :explicit, :file_url
 
+  def published_at
+    object.published_at.strftime("%d.%m.%Y")
+  end
+
   has_many :highlights, serializer: Podcast::HighlightSerializer
   belongs_to :podcast, serializer: PodcastSerializer
 end
