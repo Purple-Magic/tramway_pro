d6482f3ffc5d44146084eeda16059feae379bdb2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-05-21 03:57:58 +0400

    Upgrade

--- lib/tasks/bot_telegram/start_bots.rb
+++ lib/tasks/bot_telegram/start_bots.rb
@@ -4,8 +4,6 @@ require 'colorize'
 
 Bot.active.find_each do |bot|
   bot_listener_file = 'lib/tasks/bot_telegram/bot_listener.rb'
-  command = "RUNNING_BOT_ID=\"#{bot.id}\" /bin/bash -lic 'exec bundle exec rails runner #{bot_listener_file} &'"
-  puts command.blue
-  system command
+  BotJob.perform_later bot.id
   puts "#{bot.name} started".green
 end
