ac68bb798a8e43939cd3c84fab771e86955f934c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-06-04 01:48:32 +0400

    Fix controllers modules

--- app/controllers/red_magic/api/v1/podcast/episode/parts_controller.rb
+++ app/controllers/red_magic/api/v1/podcast/episode/parts_controller.rb
@@ -1,7 +1,16 @@
-class RedMagic::Api::V1::Podcast::Episodes::PartsController < RedMagic::Api::V1::ApplicationController
-  def index
-    episode = ::Podcast::Episode.find_by params[:id]
-    files = Dir["#{episode.parts_directory_name}/*.mp3"]
-    system "zip #{files.join(' ')}"
+module RedMagic
+  module Api
+    module V1
+      module Podcast
+        module Episodes
+          class PartsController < RedMagic::Api::V1::ApplicationController
+            def index
+              episode = ::Podcast::Episode.find_by params[:id]
+              files = Dir["#{episode.parts_directory_name}/*.mp3"]
+              system "zip #{files.join(' ')}"
+            end
+          end
+        end
+      end
+    end
   end
-end
