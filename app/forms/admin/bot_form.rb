635a173d1a7b1c7c6ac7321d5e84587fd9689992
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-07-21 03:20:36 +0400

    Add bot slug to form

--- app/forms/admin/bot_form.rb
+++ app/forms/admin/bot_form.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class Admin::BotForm < Tramway::Core::ApplicationForm
-  properties :name, :team, :project_id, :token, :options
+  properties :name, :team, :project_id, :token, :options, :slug
 
   def initialize(object)
     super(object).tap do
       form_properties name: :string,
         team: :default,
         token: :string,
+        slug: :string,
         options: :text
     end
   end
