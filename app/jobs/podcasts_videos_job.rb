9b075cb8e7a89d0cd621889afac7adbf2666828e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-07-25 10:30:52 +0400

    Fix generating video

--- app/jobs/podcasts_videos_job.rb
+++ app/jobs/podcasts_videos_job.rb
@@ -7,7 +7,7 @@ class PodcastsVideosJob < ActiveJob::Base
     episode = ::Podcast::Episode.find id
 
     episode.prepare_directory
-    command = "ffmpeg -y -loop 1 -i #{episode.cover.path} -i #{episode.ready_file.path} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest #{episode.parts_directory_name}/video.mp4"
+    command = "ffmpeg -y -loop 1 -i #{episode.cover.path} -i #{episode.ready_file.path} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest -strict -2 #{episode.parts_directory_name}/video.mp4"
     system command
   rescue StandardError => e
     Rails.env.development? ? Rails.logger.error("logger.info : #{e.message}") : Raven.capture_exception(e)
