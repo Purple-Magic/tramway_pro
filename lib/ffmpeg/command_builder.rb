35381a1983249c1d190caf33f1fac44b5d647052
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-07-26 11:27:13 +0400

    Rubocop fixes

--- lib/ffmpeg/command_builder.rb
+++ lib/ffmpeg/command_builder.rb
@@ -23,17 +23,17 @@ module Ffmpeg::CommandBuilder
     audio_codec: 'c:a',
     pixel_format: :pix_fmt,
     shortest: :boolean
-  }
+  }.freeze
 
   def build_arguments(options)
     ARGUMENTS.map do |pair|
-      if options[pair[0]].present?
-        case pair[1]
-        when :boolean
-          pair[0]
-        else
-          "#{pair[1]} #{options[pair[0]]}"
-        end
+      next unless options[pair[0]].present?
+
+      case pair[1]
+      when :boolean
+        pair[0]
+      else
+        "#{pair[1]} #{options[pair[0]]}"
       end
     end
   end
