37ae6ebc710ecf0bda4e6c6bcbff86db17a3ae7e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-07-26 08:02:50 +0400

    Rubocop fixes: add ffmpeg module

--- app/jobs/podcasts_videos_job.rb
+++ app/jobs/podcasts_videos_job.rb
@@ -7,9 +7,33 @@ class PodcastsVideosJob < ActiveJob::Base
     episode = ::Podcast::Episode.find id
 
     episode.prepare_directory
-    command = "ffmpeg -y -loop 1 -i #{episode.cover.path} -i #{episode.ready_file.path} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest -strict -2 #{episode.parts_directory_name}/video.mp4"
+    command = "ffmpeg #{options(episode)}"
+    binding.pry
+      #"-y -loop 1 -i #{episode.cover.path} -i #{episode.ready_file.path} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest -strict -2 #{output}" 
     system command
   rescue StandardError => e
     Rails.env.development? ? Rails.logger.error("logger.info : #{e.message}") : Raven.capture_exception(e)
+  end
+
+  private
+
+  include Ffmpeg::CommandBuilder
+
+  def options(episode)
+    inputs = [episode.cover.path, episode.ready_file.path]
+    output = "#{episode.parts_directory_name}/video.mp4"
+    options_line(
+      inputs: inputs,
+      output: output,
+      yes: true,
+      loop_value: 1,
+      video_codec: :libx264,
+      tune: :stillimage,
+      audio_codec: :aac,
+      bitrate_audio: '192k',
+      pixel_format: 'yuv420p',
+      shortest: true,
+      strict: 2
+    )
   end
 end
---
+++ lib/ffmpeg/command_builder.rb
@@ -0,0 +1,22 @@
+module Ffmpeg
+  module CommandBuilder
+    def options_line(inputs:, output:, **options)
+      line = options[:yes] ? '-y ' : ''
+      line += "-loop #{options[:loop_value]} " if options[:loop_value].present?
+      inputs.each do |input|
+        line += "-i #{input} "
+      end
+      arguments = []
+      arguments << "c:v #{options[:video_codec]}" if options[:video_codec].present?
+      arguments << "tune #{options[:tune]}" if options[:tune].present?
+      arguments << "c:a #{options[:audio_codec]}" if options[:audio_codec].present?
+      arguments << "pix_fmt #{options[:pixel_format]}" if options[:pixel_format].present?
+      arguments << "shortest" if options[:shortest]
+      arguments << "strict -#{options[:strict]}" if options[:strict].present?
+      arguments.each do |argument|
+        line += "-#{argument} "
+      end
+      "#{line}#{output}"
+    end
+  end
+end
