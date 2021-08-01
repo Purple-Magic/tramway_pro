2dba3c3c9b5466f2463e6b693de7c0b20801133f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-02 03:27:45 +0400

    Fix downloading external file

--- app/jobs/podcasts_download_external_file_job.rb
+++ app/jobs/podcasts_download_external_file_job.rb
@@ -1,13 +1,16 @@
+require 'net/ssh'
+
 class PodcastsDownloadExternalFileJob < ActiveJob::Base
   def perform(id)
     episode = Podcast::Episode.find id
 
     directory = episode.prepare_directory
     directory = directory.gsub("//", '/')
-    command = "ssh -t root@167.71.46.15 'ls /root/Documents/Mumble-*'"
-    result = `#{command}`
-    sleep 5
-    external_filename = result.split(' ').last.split('/').last
-    system("scp #{ENV['STREAM_SERVER_USER']}@#{ENV['STREAM_SERVER_IP']}:/root/Documents/#{external_filename} #{directory}/#{external_filename}")
+    result = ''
+    Net::SSH.start('167.71.46.15', 'root') do |ssh|
+      result = 'ls /root/Documents/Mumble-*'
+      external_filename = result.split(' ').last.split('/').last
+      system("scp root@167.71.46.15:/root/Documents/#{external_filename} #{directory}/#{external_filename}")
+    end
   end
 end
