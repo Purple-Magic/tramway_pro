d1437626de8ef0cd4b3afff388d1ff2150a97016
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-01 08:36:35 +0400

    Fix rendering

--- app/models/podcast/episode.rb
+++ app/models/podcast/episode.rb
@@ -76,7 +76,7 @@ class Podcast::Episode < ApplicationRecord
 
     # TODO: use lib/ffmpeg/builder.rb
     output = "#{directory}/montage.mp3"
-    system "ffmpeg -y -i #{filename} -af silenceremove=stop_periods=-1:stop_duration=1:stop_threshold=-30dB,acompressor=threshold=-12dB:ratio=2:attack=200:release=1000,volume=-0.5dB #{output}"
+    system "ffmpeg -y -i #{filename} -af silenceremove=stop_periods=-1:stop_duration=1:stop_threshold=-30dB,acompressor=threshold=-12dB:ratio=2:attack=200:release=1000,volume=-0.5dB -c:a libmp3lame #{output}"
 
     File.open(output) do |f|
       self.premontage_file = f
