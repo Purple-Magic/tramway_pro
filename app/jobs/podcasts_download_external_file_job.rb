099eb5d02554a38e1732be18d94000af8f8e6a77
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-02 03:02:23 +0400

    Fix exporting

--- app/jobs/podcasts_download_external_file_job.rb
+++ app/jobs/podcasts_download_external_file_job.rb
@@ -4,7 +4,7 @@ class PodcastsDownloadExternalFileJob < ActiveJob::Base
 
     directory = episode.prepare_directory
     directory = directory.gsub("//", '/')
-    command = "ssh -t #{ENV['STREAM_SERVER_USER']}@#{ENV['STREAM_SERVER_IP']} 'ls /root/Documents/Mumble-*'"
+    command = "ssh -t root@167.71.46.15 'ls /root/Documents/Mumble-*'"
     external_filename = `#{command}`.split(' ').last.split('/').last
     system("scp #{ENV['STREAM_SERVER_USER']}@#{ENV['STREAM_SERVER_IP']}:/root/Documents/#{external_filename} #{directory}/#{external_filename}")
   end
