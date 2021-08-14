01adb9319c39564334bc04fa8d3855b237ab8e85
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-14 16:36:39 +0400

    Podcast engine. Fix cutting highlights

--- app/models/podcast/episode.rb
+++ app/models/podcast/episode.rb
@@ -117,8 +117,8 @@ class Podcast::Episode < ApplicationRecord
       seconds = highlight.time.split(':')[2]
 
       highlight_time = DateTime.new(2020, 0o1, 0o1, hour.to_i, minutes.to_i, seconds.to_i)
-      begin_time = (highlight_time - 90.seconds).strftime '%H:%M:%S'
-      end_time = (highlight_time + 10.seconds).strftime '%H:%M:%S'
+      begin_time = (highlight_time - 60.seconds).strftime '%H:%M:%S'
+      end_time = (highlight_time + 30.seconds).strftime '%H:%M:%S'
       output = "#{directory}/part-#{index + 1}.mp3"
       # TODO: use lib/ffmpeg/builder.rb
       command = "ffmpeg -y -i #{filename} -ss #{begin_time} -to #{end_time} -b:a 320k -c copy #{output} 2> #{parts_directory_name}/cut_highlights-output.txt"
