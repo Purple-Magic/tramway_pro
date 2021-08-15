c1b5a6309c4908a347e9592cfa82fc6b4c181d83
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-15 23:45:08 +0400

    Decomposite finish worker methods

--- app/workers/podcasts/finish_worker.rb
+++ app/workers/podcasts/finish_worker.rb
@@ -11,16 +11,25 @@ class Podcasts::FinishWorker < ApplicationWorker
     directory = episode.prepare_directory
     directory = directory.gsub('//', '/')
 
+    concatinate_parts episode, directory
+    render_video_trailer episode, directory
+    render_full_video episode, directory
+  end
+
+  private
+
+  def concatinate_parts(episode, directory)
     output = "#{directory}/ready_file.mp3"
     episode.concat_trailer_and_episode(output)
 
-    wait_for_file_rendered output, :trailer
+    wait_for_file_rendered output, :ready_file
 
     episode.update_file! output, :ready_file
-
     episode.make_audio_ready
     episode.save!
+  end
 
+  def render_video_trailer(episode, directory)
     output = "#{directory}/trailer.mp4"
     episode.render_video_trailer(output)
 
@@ -30,7 +39,9 @@ class Podcasts::FinishWorker < ApplicationWorker
 
     episode.make_video_trailer_ready
     episode.save!
+  end
 
+  def render_full_video(episode, directory)
     output = "#{directory}/full_video.mp4"
     episode.render_full_video(output)
 
