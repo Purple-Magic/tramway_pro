7522d018c79e979513b7a10dafef41f736ff505f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-15 23:32:28 +0400

    Decompose podcast montage worker

--- app/workers/podcasts_montage_worker.rb
+++ app/workers/podcasts_montage_worker.rb
@@ -10,8 +10,18 @@ class PodcastsMontageWorker < ApplicationWorker
 
     directory = episode.prepare_directory
     directory = directory.gsub('//', '/')
+    download episode, directory
+    cut_highlights episode
+    convert episode
+    montage episode, directory
+    add_music episode, directory
+  rescue StandardError => e
+    Rails.env.development? ? Rails.logger.error("logger.info : #{e.message}") : Raven.capture_exception(e)
+  end
 
-    # Download
+  private
+
+  def download(episode, directory)
     external_filename = ''
     Net::SSH.start('167.71.46.15', 'root') do |ssh|
       result = ssh.exec! 'ls /root/Documents/Mumble-*'
@@ -26,78 +36,61 @@ class PodcastsMontageWorker < ApplicationWorker
 
     episode.download
     episode.save!
+  end
 
-    # Cut highlights
+  def cut_highlights(episode)
     episode.convert_file
     episode.cut_highlights
     episode.highlight_it
     episode.save!
+  end
 
-    # Convert
+  def convert(episode)
     filename = episode.convert_file
 
-    index = 0
-    until File.exist?(filename)
-      sleep 1
-      index += 1
-      Rails.logger.info "Convert file does not exist for #{index} seconds"
-    end
+    wait_for_file_rendered filename, :convert
 
     episode.convert
     episode.save!
+  end
 
-    # Montage
+  def montage(episode, directory)
     output = "#{directory}/montage.mp3"
 
     episode.montage(filename, output)
 
-    index = 0
-    until File.exist?(output)
-      sleep 1
-      index += 1
-      Rails.logger.info "Montage file does not exist for #{index} seconds"
-    end
+    wait_for_file_rendered output, :montage
 
-    File.open(output) do |f|
-      episode.premontage_file = f
-    end
+    save_premontage_file episode, output
 
     episode.prepare
     episode.save!
+  end
 
-    # Normalize
-    #    output = "#{directory}/normalize.mp3"
-    #    episode.normalize(episode.premontage_file.path, output)
-    #
-    #    index = 0
-    #    until File.exist?(output)
-    #      sleep 1
-    #      index += 1
-    #      Rails.logger.info "Normalized file does not exist for #{index} seconds"
-    #    end
-    #    File.open(output) do |f|
-    #      episode.premontage_file = f
-    #    end
-    #    episode.to_normalize
-    #    episode.save!
-
-    # Add music
+  def add_music(episode, directory)
     output = "#{directory}/with_music.mp3"
     episode.add_music(episode.premontage_file.path, output)
 
+    wait_for_file_rendered output, :with_music
+    save_premontage_file episode, output
+
+    episode.music_add
+    episode.save!
+  end
+
+  def wait_for_file_rendered(output, file_type)
     index = 0
     until File.exist?(output)
       sleep 1
       index += 1
-      Rails.logger.info "With music file does not exist for #{index} seconds"
+      Rails.logger.info "#{file_type} file does not exist for #{index} seconds"
     end
+  end
+
+  def save_premontage_file(episode, output)
     File.open(output) do |f|
       episode.premontage_file = f
     end
-
-    episode.music_add
     episode.save!
-  rescue StandardError => e
-    Rails.env.development? ? Rails.logger.error("logger.info : #{e.message}") : Raven.capture_exception(e)
   end
 end
