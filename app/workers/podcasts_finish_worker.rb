130b974f500e97001f67245912f700d48b056af7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-16 03:59:22 +0400

    Decompose finish worker

--- app/workers/podcasts_finish_worker.rb
+++ app/workers/podcasts_finish_worker.rb
@@ -9,53 +9,44 @@ class PodcastsFinishWorker < ApplicationWorker
     directory = episode.prepare_directory
     directory = directory.gsub('//', '/')
 
+    concat_parts directory, episode
+    render_trailer directory, episode
+    render_full_video directory, episode
+  end
+
+  private
+
+  def concat_parts(directory, episode)
     output = "#{directory}/ready_file.mp3"
     episode.concat_trailer_and_episode(output)
 
-    index = 0
-    until File.exist?(output)
-      sleep 1
-      index += 1
-      Rails.logger.info "Trailer file does not exist for #{index} seconds"
-    end
+    wait_for_file_rendered output, :trailer
 
-    File.open(output) do |f|
-      episode.ready_file = f
-    end
+    episode.update_file! output, :ready_file
 
     episode.make_audio_ready
     episode.save!
+  end
 
+  def render_trailer(directory, episode)
     output = "#{directory}/trailer.mp4"
     episode.render_video_trailer(output)
 
-    index = 0
-    until File.exist?(output)
-      sleep 1
-      index += 1
-      Rails.logger.info "Video Trailer file does not exist for #{index} seconds"
-    end
+    wait_for_file_rendered output, :video_trailer
 
-    File.open(output) do |f|
-      episode.trailer_video = f
-    end
+    episode.update_file! output, :trailer_video
 
     episode.make_video_trailer_ready
     episode.save!
+  end
 
+  def render_full_video(directory, episode)
     output = "#{directory}/full_video.mp4"
     episode.render_full_video(output)
 
-    index = 0
-    until File.exist?(output)
-      sleep 1
-      index += 1
-      Rails.logger.info "Full video file does not exist for #{index} seconds"
-    end
+    wait_for_file_rendered output, :full_video
 
-    File.open(output) do |f|
-      episode.full_video = f
-    end
+    episode.update_file! output, :full_video
 
     episode.finish
     episode.save!
