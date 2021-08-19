5b7ccc64d0279c593edaa21d931aa0adc7ef11f2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-19 09:49:41 +0400

    Rubocop fixes

--- lib/extended_logger.rb
+++ lib/extended_logger.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ExtendedLogger
   def log_error(error)
     Rails.env.development? ? Rails.logger.error("logger.info : #{error.message}") : Raven.capture_exception(error)
