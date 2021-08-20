107000e8aab5df68e4dedf37eeb5ce1e5f586dca
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-20 06:07:25 +0400

    Fix

--- app/controllers/engineervol/web/welcome_controller.rb
+++ app/controllers/engineervol/web/welcome_controller.rb
@@ -4,6 +4,22 @@ class Engineervol::Web::WelcomeController < ApplicationController
   layout 'engineervol/application'
 
   def index
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
+    fuck
     @application = Constraints::DomainConstraint.new(request.domain).application_object
     @book = Listai::Book.first
   end
