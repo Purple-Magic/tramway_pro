214e0c2ade193c87ddb0fde44c5c6d9a87fb81d1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-21 05:49:19 +0400

    Podcast engine. Fix creating topic

--- app/forms/podcast/episodes/topic_form.rb
+++ app/forms/podcast/episodes/topic_form.rb
@@ -4,7 +4,7 @@ class Podcast::Episodes::TopicForm < Tramway::Core::ApplicationForm
   properties :title, :link, :project_id
 
   def submit(params)
-    params[:episode_id] = Podcast::Episode.last.id
+    model.episode_id = Podcast::Episode.last.id
     model.save!
     model.discus
     model.save!
