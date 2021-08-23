7584eaf477194ea902d9477a40e703d6876b1453
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-23 05:48:38 +0400

    Podcast engine. Fix notifications

--- app/workers/podcasts_trailer_worker.rb
+++ app/workers/podcasts_trailer_worker.rb
@@ -5,6 +5,8 @@ class PodcastsTrailerWorker < ApplicationWorker
 
   def perform(id)
     episode = Podcast::Episode.find id
+    send_notification_to_user :kalashnikovisme, 'Podcast trailer render is started'
     episode.build_trailer
+    send_notification_to_user :kalashnikovisme, 'Podcast trailer render is ended'
   end
 end
