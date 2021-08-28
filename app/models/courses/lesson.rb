54e4130d9cbc50ae37a2405d7ff658d421057048
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-28 08:35:26 +0400

    Rubocop autofix

--- app/models/courses/lesson.rb
+++ app/models/courses/lesson.rb
@@ -10,7 +10,9 @@ class Courses::Lesson < ApplicationRecord
     started_videos = videos.active.reject { |video| video.progress_status == :not_started }
     videos_with_comments_any = videos.active.map { |video| video.comments.active.count }.uniq != [0]
 
-    return :done if started_videos.any? && started_videos.count == videos.active.count && started_videos.count == done_videos.count
+    if started_videos.any? && started_videos.count == videos.active.count && started_videos.count == done_videos.count
+      return :done
+    end
     return :in_progress if (videos.active.any? && videos_with_comments_any) || started_videos.count != done_videos.count
   end
 end
