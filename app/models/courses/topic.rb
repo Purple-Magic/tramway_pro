4b01b822a3ef8432fc0acd7a42e7fe46062ddf84
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-28 10:21:11 +0400

    Courses engine. Refactoring

--- app/models/courses/topic.rb
+++ app/models/courses/topic.rb
@@ -7,17 +7,17 @@ class Courses::Topic < ApplicationRecord
 
   def progress_status
     done_lessons = lessons.active.select { |lesson| lesson.progress_status == :done }
-    started_lessons = lessons.active.select { |lesson| lesson.progress_status == :in_progress }
+    started_lessons = lessons.active.select { |lesson| lesson.progress_status == :in_progress }.count
     lessons_with_comments_any = lessons.active.map do |lesson|
       lesson.videos.active.reject do |video|
         video.progress_status == :not_started
       end.count
     end.uniq != [0]
 
-    if started_lessons.any? && started_lessons.count == lessons.active.count && started_lessons.count == done_lessons.count
+    if started_lessons.positive? && started_lessons == lessons.active.count && started_lessons == done_lessons.count
       return :done
     end
 
-    :in_progress if (lessons.active.any? && lessons_with_comments_any) || started_lessons.count != done_lessons.count
+    :in_progress if (lessons.active.any? && lessons_with_comments_any) || started_lessons != done_lessons.count
   end
 end
