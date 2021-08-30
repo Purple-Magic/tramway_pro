28630e3091e28dc41257a027688dad8ee599239d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-31 03:45:51 +0400

    Bot engine. Upgrade stats messages

--- lib/tasks/bot_telegram/chat_quests/stats.rb
+++ lib/tasks/bot_telegram/chat_quests/stats.rb
@@ -1,22 +1,14 @@
 module BotTelegram::ChatQuests::Stats
-  CHAT_QUESTS_BOTS_IDS = [2, 3, 4, 5, 6, 8, 10, 11, 12]
+  def stats_between_dates(bot, begin_date, end_date)
+    # rubocop:disable Style/StringConcatenation
+    <<-TXT
+    🤖 #{bot.name}
+    📅 Отчёт за период с #{begin_date.strftime('%d.%m.%Y')} по #{end_date.strftime('%d.%m.%Y')}
 
-  def stats_between_dates(begin_date, end_date)
-    bots = Bot.where id: CHAT_QUESTS_BOTS_IDS
-    stat_message = "Статистика квесто в период с #{begin_date.strftime('%d.%m.%Y')} - #{end_date.strftime('%d.%m.%Y')}"
-    bots.reduce('') do |stat_message, bot|
-      # rubocop:disable Style/StringConcatenation
-      stat_message += bot.name
-      stat_message += "\n"
-      stat_message += "Общее количество пользователей: #{bot.users.count}"
-      stat_message += "\n"
-      stat_message += "Новые пользователи за период: #{bot.new_users_between(begin_date, end_date).count}"
-      stat_message += "\n"
-      stat_message += "Уникальные пользователи за период: #{bot.uniq_users_between(begin_date, end_date).count}"
-      stat_message += "\n"
-      stat_message += "Общее количество сообщений: #{bot.messages.count}"
-      stat_message + "\n"
-      # rubocop:enable Style/StringConcatenation
-    end
+    🔶 Общее количество пользователей: #{bot.users.count}
+    🔶 Новые пользователи за период: #{bot.new_users_between(begin_date, end_date).count}
+    🔶 Уникальные пользователи за период: #{bot.uniq_users_between(begin_date, end_date).count}
+    🔶 Общее количество сообщений: #{bot.messages.count}
+    TXT
   end
 end
