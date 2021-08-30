7b6a363eda28a0d2d4672b323f8f865feb8819d6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-08-31 03:16:29 +0400

    UPdate for testing

--- app/jobs/leopold_send_chat_quests_stats.rb
+++ app/jobs/leopold_send_chat_quests_stats.rb
@@ -12,9 +12,9 @@ class LeopoldSendChatQuestsStats < ActiveJob::Base
 
   def perform(*_args)
     bot = Bot.find ::BotTelegram::Leopold::Scenario::BOT_ID
-    send_notification_to_chat ::BotTelegram::Leopold::ChatDecorator::PROJECT_CHAT_QUEST_ID,
+    #send_notification_to_chat ::BotTelegram::Leopold::ChatDecorator::PROJECT_CHAT_QUEST_ID,
       'Это сообщение: проверка. В ближайшие часы буду её слать каждые 10 минут, чтобы быть уверенным, что работаю стабильно. Чуть позже, Паша, это прекратит.'
-    send_notification_to_chat ::BotTelegram::Leopold::ChatDecorator::PROJECT_CHAT_QUEST_ID,
+    send_notification_to_user :kalashnikovisme,
       stats_between_dates(DateTime.now - 1.week, DateTime.now)
   end
 end
