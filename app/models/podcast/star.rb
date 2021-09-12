51ad09f7e7ec432a43950abccbd85d8b52d91b6b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-09-12 04:51:55 +0400

    Fix relatiosn

--- app/models/podcast/star.rb
+++ app/models/podcast/star.rb
@@ -2,7 +2,6 @@
 
 class Podcast::Star < ApplicationRecord
   belongs_to :podcast, class_name: 'Podcast'
-  has_and_belongs_to_many :stars, class_name: 'Podcast::Star'
 
   scope :podcast_scope, ->(_user_id) { all }
 
