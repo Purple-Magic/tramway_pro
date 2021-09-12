ff53f647c377d66b1654e41e7611a4e39abab0ad
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-09-12 04:40:52 +0400

    Leopold. Remove IT Way Podcast process

--- lib/tasks/bot_telegram/custom/message.rb
+++ lib/tasks/bot_telegram/custom/message.rb
@@ -14,7 +14,12 @@ class BotTelegram::Custom::Message
     if @reply_markup.present?
       arguments.merge!(reply_markup: Telegram::Bot::Types::ReplyKeyboardMarkup.new(**@reply_markup))
     elsif @inline_keyboard.present?
-      arguments.merge!(reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: @inline_keyboard))
+      keyboard = @inline_keyboard.map do |button|
+        button_options = { text: button[0] }
+        button_options.merge! switch_inline_query_current_chat: button[1][:answer] if button[1][:answer].present?
+        Telegram::Bot::Types::InlineKeyboardButton.new **button_options
+      end
+      arguments.merge!(reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: keyboard))
     end
     arguments
   end
--- lib/tasks/bot_telegram/leopold/scenario.rb
+++ lib/tasks/bot_telegram/leopold/scenario.rb
@@ -9,7 +9,6 @@ class BotTelegram::Leopold::Scenario
   include ::BotTelegram::MessagesManager
   include ::BotTelegram::Info
   include ::BotTelegram::Leopold::ItWayPro
-  include ::BotTelegram::Leopold::ItWayPodcast
   include ::BotTelegram::Leopold::ChatsConcern
 
   BOT_ID = 9
@@ -27,18 +26,16 @@ class BotTelegram::Leopold::Scenario
   def run
     chat_decorator = BotTelegram::Leopold::ChatDecorator.new chat
      text = message_from_telegram.text
-#    if chat_decorator.to_answer?
-#      command = BotTelegram::Leopold::Command.new text
-#      if command.valid?
-#        command.run
-#      else
-#        it_way_process text
-#      end
-#    elsif chat_decorator.is_it_way_podcast?
-      it_way_podcast_process bot, chat, message, text
-#    else
-#      chat_id = chat.telegram_chat_id.to_s
-#      message_to_chat bot, chat, bot_record.options['not_my_group'] unless exceptions.values.include? chat_id
-#    end
+    if chat_decorator.to_answer?
+      command = BotTelegram::Leopold::Command.new text
+      if command.valid?
+        command.run
+      else
+        it_way_process text
+      end
+    else
+      chat_id = chat.telegram_chat_id.to_s
+      message_to_chat bot, chat, bot_record.options['not_my_group'] unless exceptions.values.include? chat_id
+    end
   end
 end
