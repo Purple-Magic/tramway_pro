cc93b337ad80d632d5ad1ed36b3bfaf637daecc9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-09-17 01:07:26 +0400

    Fix notifications

--- app/workers/podcasts_trailer_worker.rb
+++ app/workers/podcasts_trailer_worker.rb
@@ -8,7 +8,6 @@ class PodcastsTrailerWorker < ApplicationWorker
   def perform(id)
     chat_id = BotTelegram::Leopold::ChatDecorator::IT_WAY_PODCAST_ID
     episode = Podcast::Episode.find id
-    send_notification_to_chat chat_id, notification(:audio_trailer, :rendering_started)
     episode.build_trailer
     send_notification_to_chat chat_id, notification(:audio_trailer, :rendering_over)
     send_file_to_chat chat_id, episode.trailer.path
