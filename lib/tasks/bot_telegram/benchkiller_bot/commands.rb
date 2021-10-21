f4beadfaf6d6eb54df173808e699812fcb083cc3
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-10-21 07:39:22 +0400

    Benchkiller. Create microengine for settings actions

--- lib/tasks/bot_telegram/benchkiller_bot/commands.rb
+++ lib/tasks/bot_telegram/benchkiller_bot/commands.rb
@@ -26,32 +26,51 @@ module BotTelegram::BenchkillerBot::Commands
     message_to_user bot, message, chat.telegram_chat_id
   end
 
-  def set_company_name(argument)
-    message = 'Введите название компании'
+  SET_ACTIONS_DATA = {
+    set_company_name: {
+      message: 'Введите название компании',
+      state: :waiting_for_set_company_name
+    },
+    set_portfolio_url: {
+      message: 'Введите ссылку на портфолио',
+      state: :waiting_for_set_portfolio_url
+    },
+    set_company_url: {
+      message: 'Введите адрес сайта компании',
+      state: :waiting_for_set_company_url
+    },
+    set_email: {
+      message: 'Введите контактный email',
+      state: :waiting_for_set_email
+    },
+    set_place: {
+      message: 'Введите расположение вашей команды',
+      state: :waiting_for_set_place
+    },
+    set_phone: {
+      message: 'Введите контактный телефон',
+      state: :waiting_for_set_phone
+    },
+    set_regions_to_cooperate: {
+      message: 'Введите регионы сотрудничества',
+      state: :waiting_for_set_regions_to_cooperate
+    }
+  }
+
+  def common_set_action(action, argument)
+    message = SET_ACTIONS_DATA[action][:message]
 
     BotTelegram::Users::State.create! user_id: user.id,
       bot_id: bot_record.id,
-      current_state: :waiting_for_set_company_name
+      current_state: SET_ACTIONS_DATA[action][:state]
 
     message_to_user bot, message, chat.telegram_chat_id
   end
 
-  def set_portfolio_url(argument)
-  end
-
-  def set_company_url(argument)
-  end
-
-  def set_email(argument)
-  end
-  
-  def set_place(argument)
-  end
-
-  def set_phone(argument)
-  end
-
-  def set_regions_to_cooperate(argument)
+  ::BotTelegram::BenchkillerBot::Action::STATES_ACTIONS_RELATION.values.each do |action|
+    define_method(action) do |argument|
+      common_set_action action, argument
+    end
   end
 
   def get_company_card(argument)
