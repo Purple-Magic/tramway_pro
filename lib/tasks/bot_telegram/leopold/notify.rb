f0544684e156506fb4ad86660b7258016f6be9f7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-10-22 07:33:27 +0400

    Bot Leopold. Fix notifications

--- lib/tasks/bot_telegram/leopold/notify.rb
+++ lib/tasks/bot_telegram/leopold/notify.rb
@@ -11,13 +11,13 @@ module BotTelegram::Leopold::Notify
     bot_record = Bot.find BotTelegram::Leopold::Scenario::BOT_ID
     bot = ::Telegram::Bot::Client.new bot_record.token
     chat = BotTelegram::Chat.find_by "options ->> 'username' = '#{username}'"
-    message_to_user bot.api, message, chat.telegram_chat_id
+    message_to_user bot, message, chat.telegram_chat_id
   end
 
   def send_notification_to_chat(chat_id, message)
     bot_record = Bot.find BotTelegram::Leopold::Scenario::BOT_ID
     bot = ::Telegram::Bot::Client.new bot_record.token
-    message_to_user bot.api, message, chat_id
+    message_to_user bot, message, chat_id
   end
 
   def send_file_to_user(username, file)
@@ -25,13 +25,13 @@ module BotTelegram::Leopold::Notify
     bot = ::Telegram::Bot::Client.new bot_record.token
     chat = ::BotTelegram::Chat.find_by "options ->> 'username' = '#{username}'"
     bot_message = ::BotTelegram::Leopold::Message.new file
-    send_file bot.api, chat.telegram_chat_id, bot_message
+    send_file bot, chat.telegram_chat_id, bot_message
   end
 
   def send_file_to_chat(chat_id, file)
     bot_record = Bot.find BotTelegram::Leopold::Scenario::BOT_ID
     bot = ::Telegram::Bot::Client.new bot_record.token
     bot_message = ::BotTelegram::Leopold::Message.new file
-    send_file bot.api, chat_id, bot_message.file
+    send_file bot, chat_id, bot_message.file
   end
 end
