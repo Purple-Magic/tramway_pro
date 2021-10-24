2cf6cd8e63eaced403980f43fd56580c4baccea7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-10-24 12:39:31 +0400

    Bot Leopold. Fix sending notifications

--- lib/tasks/bot_telegram/leopold/notify.rb
+++ lib/tasks/bot_telegram/leopold/notify.rb
@@ -11,13 +11,13 @@ module BotTelegram::Leopold::Notify
     bot_record = Bot.find BotTelegram::Leopold::Scenario::BOT_ID
     bot = ::Telegram::Bot::Client.new bot_record.token
     chat = BotTelegram::Chat.find_by "options ->> 'username' = '#{username}'"
-    message_to_user bot, message, chat.telegram_chat_id
+    message_to_user bot.api, message, chat.telegram_chat_id
   end
 
   def send_notification_to_chat(chat_id, message)
     bot_record = Bot.find BotTelegram::Leopold::Scenario::BOT_ID
     bot = ::Telegram::Bot::Client.new bot_record.token
-    message_to_user bot, message, chat_id
+    message_to_user bot.api, message, chat_id
   end
 
   def send_file_to_user(username, file)
