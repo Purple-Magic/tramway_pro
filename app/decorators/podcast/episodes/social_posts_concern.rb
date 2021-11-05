f53a05a9feef9c26565c68c431694f75db880d24
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-11-05 05:04:37 +0400

    Podcast engine. Fix teelgra

--- app/decorators/podcast/episodes/social_posts_concern.rb
+++ app/decorators/podcast/episodes/social_posts_concern.rb
@@ -73,6 +73,6 @@ module Podcast::Episodes::SocialPostsConcern
     text += "\n"
     text += "Художник: @cathrinenotea"
     text += "ОБЯЗАТЕЛЬНО ПОСЛУШАЙТЕ ТРЕЙЛЕР ВЫПУСКА! Приложил его сюда :)"
-    text += "http://it-way.pro/#{object.shortened_urls.find_by(link: object.trailer_video).unique_key}"
+    text += "http://it-way.pro/#{object.shortened_urls.find_by(link: object.trailer_video.url).unique_key}"
   end
 end
