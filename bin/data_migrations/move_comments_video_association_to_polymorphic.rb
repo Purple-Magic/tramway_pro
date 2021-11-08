327c3a2ad3380bd653c69852f2650844e5fa96bb
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-11-08 07:24:53 +0400

    Benchkiller. Import messages

--- bin/data_migrations/move_comments_video_association_to_polymorphic.rb
+++
@@ -1,8 +0,0 @@
-Courses::Comment.find_each do |comment|
-  comment.update! associated_id: comment.video_id, associated_type: 'Courses::Video'
-  print "Comment #{comment.id}\r"
-end
-
-Courses::Comment.find_each do |comment|
-  puts "Comment video_id #{comment.video_id}; Associated: #{comment.associated_id}, #{comment.associated_type}"
-end
---
+++ bin/imports/benchkiller_messages.rb
@@ -0,0 +1,20 @@
+require 'csv'
+
+table = CSV.read 'messages.csv'
+users = CSV.read('persons.csv')
+table.each_with_index do |row, index|
+  next if index == 0
+  next if row[6].nil?
+  client_id = row[6].to_s
+  client = users.select do |user_row|
+    user_row[0].to_s == client_id
+  end.first
+  user_id = BotTelegram::User.find_by(telegram_id: client[1]).id
+  message_info = JSON.parse(row[2]).with_indifferent_access
+  next if message_info[:message].nil?
+  chat_id = BotTelegram::Chat.find_or_create_by!(telegram_chat_id: message_info[:message][:chat][:id],
+                                                 title: message_info[:message][:chat][:title], chat_type: row[1], project_id: 7).id
+  BotTelegram::Message.create! chat_id: chat_id, user_id: user_id, text: row[10],
+    options: message_info, project_id: 7, bot_id: 13
+  print "#{index} of #{table.count}\r"
+end
