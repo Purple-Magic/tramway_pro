db1287ab7649731fa02d9bf63635fb056feb1bf5
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-11-15 19:33:52 +0400

    Podcast engine. Fix creating episodes stuff

--- app/decorators/podcast/episode_decorator.rb
+++ app/decorators/podcast/episode_decorator.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class Podcast::EpisodeDecorator < Tramway::Core::ApplicationDecorator
-  decorate_associations :highlights, :podcast, :topics, :stars, :links, :instances
+  decorate_association :highlights, as: :episode
+  decorate_association :stars, as: :episode
+  decorate_association :podcast
+  decorate_association :topics, as: :episode
+  decorate_association :links, as: :episode
+  decorate_association :instances, as: :episode
 
   delegate_attributes :id, :number, :file_url, :montage_state
 
