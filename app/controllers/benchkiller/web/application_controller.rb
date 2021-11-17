f2df05d77840ebe73c44f331bced828d51561b06
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-11-17 05:19:26 +0400

    Benchkiller. Some updates for auth

--- app/controllers/benchkiller/web/application_controller.rb
+++ app/controllers/benchkiller/web/application_controller.rb
@@ -1,3 +1,17 @@
 class Benchkiller::Web::ApplicationController < Benchkiller::ApplicationController
   layout 'benchkiller/application'
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    if !current_user && !request.path.in?(['/'])
+      redirect_to '/'
+    end
+  end
+
+  def current_user
+    user = ::Benchkiller::User.find_by id: session['benchkiller/user_id']
+    return false unless user
+
+    ::Benchkiller::UserDecorator.decorate user
+  end
 end
