790dee17e2ff463d34823920cdce05555d7d68c1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-11-19 16:51:59 +0400

    Revert "Benchkiller. Add search"
    This reverts commit 0c5f9f47e0d72d4328404acc1ef4962ee25c5f55.

--- app/controllers/benchkiller/web/offers_controller.rb
+++ app/controllers/benchkiller/web/offers_controller.rb
@@ -1,15 +1,11 @@
 class Benchkiller::Web::OffersController < Benchkiller::Web::ApplicationController
   def index
     params[:collection] ||= :look_for
-    offers_ids = if params[:collection] == 'all'
-      ::Benchkiller::Offer.active.map(&:id)
+    if params[:collection] == 'all'
+      @offers = ::Benchkiller::OfferDecorator.decorate ::Benchkiller::Offer.active.page params[:page]
     else
-      ::Benchkiller::Tag.active.includes(:offers).find_by(title: params[:collection]).offers.map(&:id)
+      offers_ids = ::Benchkiller::Tag.active.includes(:offers).find_by(title: params[:collection]).offers.map &:id
+      @offers = ::Benchkiller::OfferDecorator.decorate ::Benchkiller::Offer.where(id: offers_ids).page params[:page]
     end
-    offers = ::Benchkiller::Offer.where(id: offers_ids)
-    if params[:search].present?
-      offers = offers.full_text_search params[:search]
-    end
-    @offers = ::Benchkiller::OfferDecorator.decorate offers.page params[:page]
   end
 end
