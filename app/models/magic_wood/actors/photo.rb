39c263999f4b4e73fb9f2f38a2fb24a8e0313f4e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-11-30 02:18:25 +0400

    MagicWood engine. Add avatar to actors

--- app/models/magic_wood/actors/photo.rb
+++ app/models/magic_wood/actors/photo.rb
@@ -1,5 +1,5 @@
 class MagicWood::Actors::Photo < ApplicationRecord
   belongs_to :actor, class_name: 'MagicWood::Actor'
 
-  uploader :file, :photo, versions: [ :medium ]
+  uploader :file, :photo, versions: [ :medium, :small ]
 end
