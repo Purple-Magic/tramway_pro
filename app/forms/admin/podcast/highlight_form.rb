6dc0cf3a04c84a2effd8e1af1615fb1d14f38c27
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-12-12 23:06:43 +0400

    Podcast engine. Add time to edit form of highlights

--- app/forms/admin/podcast/highlight_form.rb
+++ app/forms/admin/podcast/highlight_form.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class Admin::Podcast::HighlightForm < Tramway::Core::ApplicationForm
-  properties :using_state, :cut_begin_time, :cut_end_time, :trailer_position, :project_id
+  properties :using_state, :cut_begin_time, :cut_end_time, :trailer_position, :project_id, :time
 
   def initialize(object)
     super(object).tap do
       form_properties using_state: :default,
         cut_begin_time: :string,
         cut_end_time: :string,
-        trailer_position: :numeric
+        trailer_position: :numeric,
+        time: :string
     end
   end
 end
