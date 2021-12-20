393ebbb1e1f1d9fb79492187a113c5667bac40d1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-12-20 23:51:21 +0400

    Podcast engine. Add notification after render

--- app/workers/podcasts/render_video_worker.rb
+++ app/workers/podcasts/render_video_worker.rb
@@ -18,14 +18,7 @@ class Podcasts::RenderVideoWorker < ApplicationWorker
   private
 
   def render_video(episode)
-    render_full_video episode
-    send_notification_to_chat episode.podcast.chat_id, notification(:video, :finished, file_url: episode.full_video.url)
-  end
-
-  def render_full_video(episode)
     output = "#{@directory}/full_video.mp4"
     episode.render_full_video(output)
-
-    Rails.logger.info 'Render full video completed'
   end
 end
--- lib/podcasts.rb
+++ lib/podcasts.rb
@@ -1,2 +1,5 @@
 module Podcasts
+  def notification(process, event, **options)
+    I18n.t("podcast_engine.notifications.#{process}.#{event}", **options)
+  end
 end
