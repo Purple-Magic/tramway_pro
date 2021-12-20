df0f679b502782fc3c3ae8bc913a905188a27714
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-12-20 17:16:31 +0400

    Benchkiller. Fix delivery send to me

--- app/models/benchkiller/delivery.rb
+++ app/models/benchkiller/delivery.rb
@@ -31,6 +31,6 @@ class Benchkiller::Delivery < ApplicationRecord
   end
 
   def send_to_me
-    ::Benchkiller::DeliveryWorker.new.perform [bot_telegram_user_id], text
+    ::Benchkiller::DeliveryWorker.new.perform [benchkiller_user.bot_telegram_user_id], text
   end
 end
