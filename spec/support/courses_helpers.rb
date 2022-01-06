ea0fec3c20cc472a457d2f9ce7b3174b63cf854c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-01-06 11:58:54 +0400

    Rubocop fix courses_helpers

--- spec/support/courses_helpers.rb
+++ spec/support/courses_helpers.rb
@@ -3,11 +3,14 @@
 module CoursesHelpers
   def associated_title(comment)
     associated = comment.associated
+    associated_position = "#{associated.lesson.topic.position}-#{associated.lesson.position}-#{associated.position}"
+    associated_comments = "#{associated.comments.count} comments | #{associated.comments.done.count} comments done"
+    common_part = "#{associated.model_name.human} | #{associated_position} | #{associated_comments}"
     case comment.associated_type
     when 'Courses::Video'
-      "#{associated.model_name.human} #{associated.lesson.topic.position}-#{associated.lesson.position}-#{associated.position} | #{associated.comments.count} comments | #{associated.comments.done.count} comments done | #{associated.duration}"
+      "#{common_part} | #{associated.duration}"
     when 'Courses::Task'
-      "#{associated.model_name.human} #{associated.lesson.topic.position}-#{associated.lesson.position}-#{associated.position} | #{associated.comments.count} comments | #{associated.comments.done.count} comments done"
+      common_part
     end
   end
 end
