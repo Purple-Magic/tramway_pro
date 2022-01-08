591f5835328fd23f383d98626c132730140680df
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-01-08 08:18:49 +0400

    Bot engine. Add full scenario test

--- spec/factories/bots.rb
+++ spec/factories/bots.rb
@@ -11,12 +11,12 @@ FactoryBot.define do
     factory :bot_with_start_step, traits: [ :with_start_step ]
 
     trait :quest do
-#      steps = YAML.load_file("#{Rails.root}/support/quests/test_quest.yml")
-#      after :create do
-#        steps.each do |current_step|
-#          create :bot_telegram_scenario_step, **current_step
-#        end
-#      end
+      steps = YAML.load_file("#{Rails.root}/spec/support/quests/test_quest.yml").with_indifferent_access[:steps]
+      after :create do |bot|
+        steps.each do |current_step|
+          bot.steps.create!(**attributes_for(:bot_telegram_scenario_step), **current_step)
+        end
+      end
     end
 
     trait :with_start_step do
