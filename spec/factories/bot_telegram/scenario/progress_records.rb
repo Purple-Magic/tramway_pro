2f321228c77db7c2f4a74aac470f266bfea5fabe
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-01-14 20:37:06 +0400

    Bot engine. Fix tests

--- spec/factories/bot_telegram/scenario/progress_records.rb
+++ spec/factories/bot_telegram/scenario/progress_records.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :bot_telegram_scenario_process_record, class: 'BotTelegram::Scenario::ProgressRecord' do
     user { create :bot_telegram_user }
     step { create :bot_telegram_scenario_step }
-    project_id { Project.find_by(title: 'RedMagic').id }
+    project_id { Project.find_by(title: 'Benchkiller').id }
   end
 end
