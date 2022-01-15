ad492689519123d892f8c7342d78bbc0d1745700
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-01-16 00:56:59 +0400

    Benchkiller. More bot tests

--- spec/features/benchkiller/bot/changing_company_info_spec.rb
+++ spec/features/benchkiller/bot/changing_company_info_spec.rb
@@ -5,30 +5,60 @@ describe 'BotTelegram::BenchkillerBot' do
     let!(:bot_record) { create :benchkiller_bot }
     let(:chat) { create :bot_telegram_private_chat, bot: bot_record }
     let(:message_object) { create :bot_telegram_message }
+    let!(:company) do
+      user = create :benchkiller_user, telegram_user: message_object.user, password: '123'
+      company = create :benchkiller_company
+      company.users << user
+      company.save!
+      company
+    end
 
-    ::BotTelegram::BenchkillerBot::Command::COMMANDS.each do |command|
-      unless command.in? [ :start, :get_company_card, :create_password, :approve_offer, :decline_offer ]
-        describe command.to_s.capitalize.gsub('_', ' ') do
-          let(:callback_query) { build "#{command}_telegram_callback_query" }
+    before  do
+    end
 
-          it 'returns message' do
-            stub = send_markdown_message_stub_request body: {
-              chat_id: chat.telegram_chat_id,
-              text: ::BotTelegram::BenchkillerBot::ACTIONS_DATA[command][:message]
-            }
+    describe 'Callback queries' do
+      ::BotTelegram::BenchkillerBot::Command::COMMANDS.each do |command|
+        unless command.in? [ :start, :create_password, :approve_offer, :decline_offer ]
+          describe command.to_s.capitalize.gsub('_', ' ') do
+            let(:callback_query) { build "#{command}_telegram_callback_query" }
 
-            Telegram::Bot::Client.run(bot_record.token) do |bot|
-              BotTelegram::BenchkillerBot::Scenario.new(
-                callback_query,
-                bot,
-                bot_record,
-                chat,
-                message_object,
-                message_object.user
-              ).run
+            it 'returns message' do
+              card = ::Benchkiller::CompanyDecorator.decorate(company).bot_card
+
+              card_stub = send_markdown_message_stub_request body: {
+                chat_id: chat.telegram_chat_id,
+                text: card
+              }
+
+              stubs = [ card_stub ]
+
+              unless command == :get_company_card
+                message = ::BotTelegram::BenchkillerBot::ACTIONS_DATA[command][:message]
+
+                set_command_stub = send_markdown_message_stub_request body: {
+                  chat_id: chat.telegram_chat_id,
+                  text: message
+                }
+                stubs << set_command_stub
+
+
+              end
+
+              Telegram::Bot::Client.run(bot_record.token) do |bot|
+                BotTelegram::BenchkillerBot::Scenario.new(
+                  callback_query,
+                  bot,
+                  bot_record,
+                  chat,
+                  message_object,
+                  message_object.user
+                ).run
+              end
+
+              stubs.each do |stub|
+                expect(stub).to have_been_requested
+              end
             end
-
-            expect(stub).to have_been_requested
           end
         end
       end
