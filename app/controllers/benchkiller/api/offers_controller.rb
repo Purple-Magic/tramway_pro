c4c055b842b28da0d0b48ea5b3f375ec75d52b8b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-01-15 14:57:21 +0400

    Benchkiller. Refactoring

--- app/controllers/benchkiller/api/offers_controller.rb
+++ app/controllers/benchkiller/api/offers_controller.rb
@@ -5,18 +5,7 @@ class Benchkiller::Api::OffersController < Benchkiller::Api::ApplicationControll
     params[:collection] ||= :lookfor
     head 406 unless params[:collection].in?(::Benchkiller::Offer::AVAILABLE_SCOPES)
 
-    offers = ::Benchkiller::Offer.public_send(params[:collection])
-    offers = Benchkiller::Offers::SearchService.new(params[:search], offers, :query).call
-    offers = Benchkiller::Offers::SearchService.new(params[:regions], offers, :regions).call
-    offers = Benchkiller::Offers::SearchService.new(
-      {
-        begin_date: params[:begin_date],
-        end_date: params[:end_date]
-      },
-      offers,
-      :period
-    ).call
-    @full_offers_collection = offers.approved.order(created_at: :desc)
+    search_offers
 
     render json: @full_offers_collection.page(params[:page]),
       each_serializer: ::Benchkiller::OfferSerializer,
