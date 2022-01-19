c1168a26498e61ba826406b4cca0e3c9f4bf3c25
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-01-19 10:58:08 +0400

    Bot engine. Fix custom scenario

--- lib/tasks/bot_telegram/custom/scenario.rb
+++ lib/tasks/bot_telegram/custom/scenario.rb
@@ -8,10 +8,10 @@ class BotTelegram::Custom::Scenario
 
   def initialize(**options)
     @bot = options[:bot]
-    @message_from_telegram = options[:message_from_telegram]
+    @message_from_telegram = options[:message]
     @bot_record = options[:bot_record]
     @chat = options[:chat]
-    @message = options[:message]
+    @message = options[:message_object]
     @user = options[:user]
   end
 end
