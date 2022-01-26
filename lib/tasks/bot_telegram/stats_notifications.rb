ff5aa173f3945933b8157793f030786ca5c30699
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-01-26 13:04:52 +0400

    Bot engine. Add @ to notifying about finishing scenario

--- lib/tasks/bot_telegram/stats_notifications.rb
+++ lib/tasks/bot_telegram/stats_notifications.rb
@@ -6,7 +6,7 @@ module BotTelegram::StatsNotifications
 
   def notify_about_finishing_scenario(message_from_telegram, bot_record)
     user = user_from message_from_telegram.from
-    message = "Пользователь #{user.username} прошёл квест #{bot_record.name}"
+    message = "Пользователь @#{user.username} прошёл квест #{bot_record.name}"
     chat_id = ::BotTelegram::Leopold::ChatDecorator::PROJECT_CHAT_QUEST_ID
     send_notification_to_chat chat_id, message
   end
