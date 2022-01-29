3ab431f95425b0692eabf29e1ed1ceacf339f108
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-01-29 19:32:06 +0400

    Courses engine. Add timelog button

--- app/decorators/courses/video_decorator.rb
+++ app/decorators/courses/video_decorator.rb
@@ -71,13 +71,23 @@ class Courses::VideoDecorator < ApplicationDecorator
   alias name title
 
   def additional_buttons
-    add_scenario_step_url = Tramway::Admin::Engine.routes.url_helpers.new_record_path(
+    add_comment_url = Tramway::Admin::Engine.routes.url_helpers.new_record_path(
       model: 'Courses::Comment',
       'courses/comment' => { associated_type: object.class.to_s, associated: object.id },
       redirect: "/admin/records/#{object.id}?model=Courses::Video"
     )
+    add_time_log_url = Tramway::Admin::Engine.routes.url_helpers.new_record_path(
+      model: 'TimeLog',
+      'time_log' => { associated_type: object.class.to_s, associated: object.id },
+      redirect: "/admin/records/#{object.id}?model=Courses::Video"
+    )
 
-    { show: [{ url: add_scenario_step_url, inner: -> { fa_icon 'comment' }, color: :success }] }
+    {
+      show: [
+        { url: add_comment_url, inner: -> { fa_icon 'comment' }, color: :success },
+        { url: add_time_log_url, inner: -> { fa_icon 'clock' }, color: :success }
+      ]
+    }
   end
 
   def link
