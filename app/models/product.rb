16d51db77b6d023dc5df5df8842482189e6aa934
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-02-06 11:20:23 +0400

    Product engine. Change tasks order

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-  has_many :tasks, class_name: 'Products::Task', dependent: :destroy
+  has_many :tasks, -> { order :id }, class_name: 'Products::Task', dependent: :destroy
   has_many :time_logs, through: :tasks
 end
