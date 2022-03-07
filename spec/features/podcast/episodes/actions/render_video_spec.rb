794ef09e6e5195cd535a56b4b2cfce7c5fcd5799
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-03-07 23:32:50 +0400

    Test refactorng

--- spec/features/podcast/episodes/actions/render_video_spec.rb
+++ spec/features/podcast/episodes/actions/render_video_spec.rb
@@ -12,16 +12,19 @@ describe 'Render video' do
 
   it 'sends command to a remote server' do
     connect_info = 'root@82.148.30.250'
-    command_1 = ssh_run connect_info, "mkdir podcast_engine/#{episode.id}"
-    command_2 = ssh_run connect_info,
-      command(name: :nohup, command: command(name: :merge_image_and_audio, episode_id: episode.id.to_s))
-    command_3 = scp_run episode.cover.path, connect_info, "/root/podcast_engine/#{episode.id}/"
-    command_4 = scp_run episode.ready_file.path, connect_info, "/root/podcast_engine/#{episode.id}/"
+    commands = [
+      ssh_run(connect_info, "mkdir podcast_engine/#{episode.id}"),
+      ssh_run(
+        connect_info,
+        command(name: :nohup, command: command(name: :merge_image_and_audio, episode_id: episode.id.to_s))
+      ),
+      scp_run(episode.cover.path, connect_info, "/root/podcast_engine/#{episode.id}/"),
+      scp_run(episode.ready_file.path, connect_info, "/root/podcast_engine/#{episode.id}/")
+    ]
 
-    expect_any_instance_of(Kernel).to receive(:system).with(command_1)
-    expect_any_instance_of(Kernel).to receive(:system).with(command_2)
-    expect_any_instance_of(Kernel).to receive(:system).with(command_3)
-    expect_any_instance_of(Kernel).to receive(:system).with(command_4)
+    commands.each do |command|
+      expect_any_instance_of(Kernel).to receive(:system).with(command)
+    end
 
     Podcasts::RenderVideoWorker.new.perform episode.id
   end
