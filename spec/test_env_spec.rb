293408363f394a9097792505caf22417d07cf991
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-03-09 00:19:32 +0400

    Test tests

--- spec/test_env_spec.rb
+++ spec/test_env_spec.rb
@@ -2,10 +2,20 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Testing environment' do
+describe 'Testing environment' do
   describe 'working' do
     it 'should just run test' do
       expect(true).to be_truthy
+    end
+  end
+
+  describe 'test browser' do
+    it 'should default page' do
+      move_host_to 'localhost'
+
+      visit '/'
+
+      expect(page).to be_truthy
     end
   end
 end
