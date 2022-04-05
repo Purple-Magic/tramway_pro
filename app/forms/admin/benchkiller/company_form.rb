364916bcfa6e9e0e548391d56cb87986fc72bdfb
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-04-05 16:46:28 +0400

    Benchkiller. Add regions to except

--- app/forms/admin/benchkiller/company_form.rb
+++ app/forms/admin/benchkiller/company_form.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Admin::Benchkiller::CompanyForm < Tramway::Core::ApplicationForm
-  properties :title, :email, :phone, :place, :portfolio_url, :company_url, :regions_to_cooperate, :project_id
+  properties :title, :email, :phone, :place, :portfolio_url, :company_url, :regions_to_cooperate, :project_id, :regions_to_except
 
   def initialize(object)
     super(object).tap do
@@ -11,7 +11,8 @@ class Admin::Benchkiller::CompanyForm < Tramway::Core::ApplicationForm
         place: :string,
         company_url: :string,
         portfolio_url: :string,
-        regions_to_cooperate: :string
+        regions_to_cooperate: :string,
+        regions_to_except: :string
     end
   end
 end
