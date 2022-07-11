b4fdc5c022b85ca73330285ec548b82564eb62ef
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-07-11 21:52:34 +0400

    Fix creating codes

--- app/controllers/youtube_callbacks_controller.rb
+++ app/controllers/youtube_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class YoutubeCallbacksController < ApplicationController
   def create
-    yt = Youtube::Account.new authorization_code: params[:authorization_code]
+    yt = Youtube::Account.new code: params[:authorization_code]
     yt.save!
     redirect_to root_path
   end
