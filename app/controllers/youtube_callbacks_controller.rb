b732aeeade4562ac68c92b87a710cdcdf67c8244
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-07-11 21:51:34 +0400

    Fix routes

--- app/controllers/youtube_callbacks_controller.rb
+++ app/controllers/youtube_callbacks_controller.rb
@@ -2,5 +2,6 @@ class YoutubeCallbacksController < ApplicationController
   def create
     yt = Youtube::Account.new authorization_code: params[:authorization_code]
     yt.save!
+    redirect_to root_path
   end
 end
