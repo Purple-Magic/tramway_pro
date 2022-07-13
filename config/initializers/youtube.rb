f40a6ed1bd1ee57066fcdbc306ba4528a2d94cf6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-07-14 03:13:53 +0400

    Podcast engine. Fix youtube debugger

--- config/initializers/youtube.rb
+++ config/initializers/youtube.rb
@@ -2,4 +2,5 @@ Yt.configure do |config|
   config.api_key = ENV['YT_API_KEY']
   config.client_id = ENV['YT_CLIENT_ID']
   config.client_secret = ENV['YT_CLIENT_SECRET']
+  config.log_level = :debug
 end
