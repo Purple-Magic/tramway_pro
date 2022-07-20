862c0e2b70ce42948a1f4dab67238b12f49524d4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-07-20 19:15:16 +0400

    Product engine. Add tasks sum to table

--- app/decorators/product/tasks_xls_decorator.rb
+++ app/decorators/product/tasks_xls_decorator.rb
@@ -11,9 +11,11 @@ class Product::TasksXlsDecorator < Tramway::Export::Xls::ApplicationDecorator
     end
   end
 
+  include TimeManager
+
   def flexible_columns
     [{ title: -> { object.title } }] + object.product.time_logs.map(&:user).flatten.map do |user|
       { user.full_name => -> { TimeLog.time_logged_by(user, object) }  } 
-    end
+    end + [{ sum: -> { minutes_to_hours(object.time_logs.sum { |t| t.minutes_of(t.time_spent) }) } }]
   end
 end
