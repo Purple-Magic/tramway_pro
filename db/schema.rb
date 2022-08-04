b2f3d1ab5b8fe844375a21be3ce16d850ecb9cfb
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-08-05 02:51:59 +0400

    Podcast engine. Add podcast stats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220720143012) do
+ActiveRecord::Schema.define(version: 20220804213922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -269,6 +269,14 @@ ActiveRecord::Schema.define(version: 20220720143012) do
     t.integer "chapter_id"
   end
 
+  create_table "chatquestulsk_games", force: :cascade do |t|
+    t.text "area"
+    t.integer "bot_telegram_user_id"
+    t.text "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string "data_file_name", null: false
     t.string "data_content_type"
@@ -386,6 +394,17 @@ ActiveRecord::Schema.define(version: 20220720143012) do
     t.datetime "deleted_at"
     t.text "result_duration"
     t.text "url"
+  end
+
+  create_table "elections_candidates", force: :cascade do |t|
+    t.text "full_name"
+    t.text "description"
+    t.integer "area"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "consignment"
+    t.text "state"
+    t.integer "project_id"
   end
 
   create_table "estimation_coefficients", force: :cascade do |t|
@@ -668,6 +687,24 @@ ActiveRecord::Schema.define(version: 20220720143012) do
     t.text "first_name"
     t.text "last_name"
     t.datetime "deleted_at"
+  end
+
+  create_table "podcast_stats", force: :cascade do |t|
+    t.integer "month"
+    t.integer "year"
+    t.integer "podcast_id"
+    t.text "service"
+    t.integer "downloads"
+    t.integer "streams"
+    t.integer "listeners"
+    t.float "hours"
+    t.float "average_listenning"
+    t.float "overhearing_percent"
+    t.datetime "deleted_at"
+    t.integer "project_id"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "podcasts", force: :cascade do |t|
