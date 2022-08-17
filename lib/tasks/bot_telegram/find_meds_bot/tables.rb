5d830fb1a457d452739081677ec9b47834b9e90f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-08-17 16:24:59 +0400

    Find Meds. Fix starting

--- lib/tasks/bot_telegram/find_meds_bot/tables.rb
+++ lib/tasks/bot_telegram/find_meds_bot/tables.rb
@@ -1,9 +1,9 @@
-# FIXME for some reason these lines create errors
-unless Rails.env.test?
-  require_relative 'tables/application_table'
-  require_relative 'tables/main'
-  require_relative 'tables/medicine'
-end
+# # FIXME for some reason these lines create errors
+# unless Rails.env.test?
+#   require_relative 'tables/application_table'
+#   require_relative 'tables/main'
+#   require_relative 'tables/medicine'
+# end
 
 module BotTelegram::FindMedsBot::Tables
 end
