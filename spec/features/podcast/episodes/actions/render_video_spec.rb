ff1c9cdbc6e8ae39157bc6a19ad25e0115b7b787
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-08-18 06:45:17 +0400

    Remove unused tests

--- spec/features/podcast/episodes/actions/render_video_spec.rb
+++ spec/features/podcast/episodes/actions/render_video_spec.rb
@@ -1,35 +1,35 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'rails_helper'
+# require 'rails_helper'
 
-describe 'Render video' do
-  before { move_host_to it_way_host }
+# describe 'Render video' do
+#   before { move_host_to it_way_host }
 
-  let!(:episode) do
-    podcast = create :podcast, project_id: it_way_id, podcast_type: :sample
-    create :podcast_episode, podcast: podcast, project_id: it_way_id, cover: generate(:image_as_file),
-      ready_file: generate(:sound_as_file)
-  end
+#   let!(:episode) do
+#     podcast = create :podcast, project_id: it_way_id, podcast_type: :sample
+#     create :podcast_episode, podcast: podcast, project_id: it_way_id, cover: generate(:image_as_file),
+#       ready_file: generate(:sound_as_file)
+#   end
 
-  it 'sends command to a remote server' do
-    connect_info = 'root@82.148.30.250'
-    commands = [
-      ssh_run(connect_info, "mkdir podcast_engine/#{episode.id}"),
-      ssh_run(
-        connect_info,
-        command(name: :nohup, command: command(name: :merge_image_and_audio, episode_id: episode.id.to_s))
-      ),
-      scp_run(episode.cover.path, connect_info, "/root/podcast_engine/#{episode.id}/"),
-      scp_run(episode.ready_file.path, connect_info, "/root/podcast_engine/#{episode.id}/")
-    ]
+#   it 'sends command to a remote server' do
+#     connect_info = 'root@82.148.30.250'
+#     commands = [
+#       ssh_run(connect_info, "mkdir podcast_engine/#{episode.id}"),
+#       ssh_run(
+#         connect_info,
+#         command(name: :nohup, command: command(name: :merge_image_and_audio, episode_id: episode.id.to_s))
+#       ),
+#       scp_run(episode.cover.path, connect_info, "/root/podcast_engine/#{episode.id}/"),
+#       scp_run(episode.ready_file.path, connect_info, "/root/podcast_engine/#{episode.id}/")
+#     ]
 
-    commands.each do |command|
-      expect_any_instance_of(Kernel).to receive(:system).with(command)
-    end
+#     commands.each do |command|
+#       expect_any_instance_of(Kernel).to receive(:system).with(command)
+#     end
 
-    sleep 1
-    episode.reload
+#     sleep 1
+#     episode.reload
 
-    Podcasts::RenderVideoWorker.new.perform episode.id
-  end
-end
+#     Podcasts::RenderVideoWorker.new.perform episode.id
+#   end
+# end
