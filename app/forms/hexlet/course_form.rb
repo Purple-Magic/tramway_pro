0de507bb282b4e935b7f0b0c7fd40a0706d4a8cd
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-08-18 05:35:32 +0400

    Courses engine. Fix Hexlet team

---
+++ app/forms/hexlet/course_form.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Hexlet::CourseForm < Tramway::Core::ApplicationForm
+  properties :title, :project_id, :team
+
+  def initialize(object)
+    super(object).tap do
+      form_properties title: :string,
+        team: :default
+    end
+  end
+end
---
+++ app/forms/hexlet/courses/screencast_form.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Hexlet::Courses::ScreencastForm < Tramway::Core::ApplicationForm
+  properties :project_id, :scenario, :begin_time, :end_time, :file, :comment
+
+  association :video
+
+  def initialize(object)
+    super(object).tap do
+      form_properties video: :association,
+        begin_time: :string,
+        end_time: :string,
+        file: :file,
+        comment: :string
+    end
+  end
+end
