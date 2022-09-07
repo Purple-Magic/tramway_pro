f73373132a583c65b3f62a22e2011cbe518368dc
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-09-08 00:23:37 +0400

    Kalashnikovisme. Fix getting podcasts

--- app/controllers/kalashnikovisme/web/welcome_controller.rb
+++ app/controllers/kalashnikovisme/web/welcome_controller.rb
@@ -5,6 +5,6 @@ class Kalashnikovisme::Web::WelcomeController < Tramway::Core::ApplicationContro
 
   def index
     @podcast = Podcast.unscoped.find(2)
-    @links = (Blogs::Link.all + @podcast.episodes.unscoped.reverse)
+    @links = (Blogs::Link.all + @podcast.episodes.unscoped.where.not(public_title: nil).reverse)
   end
 end
