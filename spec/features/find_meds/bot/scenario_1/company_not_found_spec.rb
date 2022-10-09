e64efcea25a1bf242447113dafaf016360cba9c6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-10-10 01:28:20 +0400

    FindMeds. Test refactoring

--- spec/features/find_meds/bot/scenario_1/company_not_found_spec.rb
+++ spec/features/find_meds/bot/scenario_1/company_not_found_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative './shared/scenario_1/success'
+require_relative './shared/scenario_1/failure'
 
 describe 'BotTelegram::FindMedsBot' do
   let!(:bot_record) { create :find_meds_bot }
@@ -11,148 +13,27 @@ describe 'BotTelegram::FindMedsBot' do
   end
 
   context 'Scenario 1' do
-    describe 'Company Not Found' do
-      let(:message_1) { build :telegram_message, text: 'Поиск лекарств' }
-      let(:message_2) { build :telegram_message, text: 'Финлепсин Ретард' }
-      let(:message_3) { build :telegram_message, text: 'Нужной фирмы нет' }
-      let(:message_4) { build :telegram_message, text: 'NOVARTIS FARMA, S.p.A.' }
-      let(:message_5) { build :telegram_message, text: 'В начало' }
+    include_context 'FindMeds Scenario 1 Success'
+    include_context 'FindMeds Scenario 1 Failure'
 
+    describe 'Company Not Found' do
       context 'Saving feedback' do
         let!(:bot_leopold) { create :leopold_bot }
 
         it 'returns not found message' do
-          find_meds_airtable_stub_request table: :drugs
-          find_meds_airtable_stub_request table: :medicines
-          find_meds_airtable_stub_request table: :companies
-          find_meds_airtable_stub_request table: :companies, id: 'receQeH2nuPmxUA7P'
-          find_meds_airtable_stub_request table: :companies, id: 'receQeH2nuPmxUA7L'
-
-          stub_1 = send_message_stub_request body: {
-            chat_id: chat.telegram_chat_id,
-            text: 'Убедитесь, что название написано правильно'
-          }
-
-          bot_run :find_meds, bot_record: bot_record, message: message_1, chat: chat, message_object: message_object
-
-          expect(stub_1).to have_been_requested
-
-          stub_2 = send_message_stub_request body: {
-            chat_id: chat.telegram_chat_id,
-            text: 'Мы нашли лекараство. Лекарством какой фирмы вы пользуетесь?',
-            reply_markup: reply_markup(
-              [
-                'NOVARTIS FARMA, S.p.A.',
-                'МОСКОВСКИЙ ЭНДОКРИННЫЙ ЗАВОД, ФГУП'
-              ],
-              [
-                'В начало',
-                'Нужной фирмы нет'
-              ]
-            )
-          }
-
-          bot_run :find_meds, bot_record: bot_record, message: message_2, chat: chat, message_object: message_object
-
-          expect(stub_2).to have_been_requested
-
-          stub_3 = send_message_stub_request body: {
-            chat_id: chat.telegram_chat_id,
-            text: 'Увы, у нас пока нет информации о других компаниях, выпускающих это лекарство.',
-            reply_markup: reply_markup(
-              ['В начало']
-            )
-          }
-
-          bot_run :find_meds, bot_record: bot_record, message: message_3, chat: chat, message_object: message_object
-
-          expect(stub_3).to have_been_requested
-
-          stub_4 = send_message_stub_request body: {
-            chat_id: chat.telegram_chat_id,
-            text: 'Мы приняли информацию!',
-            reply_markup: reply_markup(
-              ['В начало']
-            )
-          }
-
-          feedback_id = FindMeds::Feedback.last.id + 1
-          stub_5 = send_message_stub_request(
-            body: {
-              chat_id: ::BotTelegram::FindMedsBot::DEVELOPER_CHAT,
-              text: "Мы получили новую обратную связь от пользователя. Посмотреть её можно здесь http://purple-magic.com/admin/records/#{feedback_id}?model=FindMeds::Feedback"
-            },
-            current_bot: bot_leopold
-          )
-
-          bot_run :find_meds, bot_record: bot_record, message: message_4, chat: chat, message_object: message_object
-
-          expect(stub_4).to have_been_requested
-          expect(stub_5).to have_been_requested
-          expect(FindMeds::Feedback.last.text).to eq message_4.text
+          push_search_medicine_button
+          type_existing_medicine
+          type_not_existing_company
+          type_feedback
         end
       end
 
       context 'Returning to the beginning' do
         it 'returns the start message' do
-          find_meds_airtable_stub_request table: :drugs
-          find_meds_airtable_stub_request table: :medicines
-          find_meds_airtable_stub_request table: :companies
-          find_meds_airtable_stub_request table: :companies, id: 'receQeH2nuPmxUA7P'
-          find_meds_airtable_stub_request table: :companies, id: 'receQeH2nuPmxUA7L'
-
-          stub_1 = send_message_stub_request body: {
-            chat_id: chat.telegram_chat_id,
-            text: 'Убедитесь, что название написано правильно'
-          }
-
-          bot_run :find_meds, bot_record: bot_record, message: message_1, chat: chat, message_object: message_object
-
-          expect(stub_1).to have_been_requested
-
-          stub_2 = send_message_stub_request body: {
-            chat_id: chat.telegram_chat_id,
-            text: 'Мы нашли лекараство. Лекарством какой фирмы вы пользуетесь?',
-            reply_markup: reply_markup(
-              [
-                'NOVARTIS FARMA, S.p.A.',
-                'МОСКОВСКИЙ ЭНДОКРИННЫЙ ЗАВОД, ФГУП'
-              ],
-              [
-                'В начало',
-                'Нужной фирмы нет'
-              ]
-            )
-          }
-
-          bot_run :find_meds, bot_record: bot_record, message: message_2, chat: chat, message_object: message_object
-
-          expect(stub_2).to have_been_requested
-
-          stub_3 = send_message_stub_request body: {
-            chat_id: chat.telegram_chat_id,
-            text: 'Увы, у нас пока нет информации о других компаниях, выпускающих это лекарство.',
-            reply_markup: reply_markup(
-              ['В начало']
-            )
-          }
-
-          bot_run :find_meds, bot_record: bot_record, message: message_3, chat: chat, message_object: message_object
-
-          expect(stub_2).to have_been_requested
-
-
-          stub_3 = send_message_stub_request body: {
-            chat_id: chat.telegram_chat_id,
-            text: 'Выберите следующее действие',
-            reply_markup: reply_markup([
-              'Поиск лекарств', 'О проекте'
-            ])
-          }
-
-          bot_run :find_meds, bot_record: bot_record, message: message_5, chat: chat, message_object: message_object
-
-          expect(stub_3).to have_been_requested
+          push_search_medicine_button
+          type_existing_medicine
+          type_not_existing_company
+          push_to_beginning_button
         end
       end
     end
--- spec/features/find_meds/bot/scenario_1/medicine_found_spec.rb
+++ spec/features/find_meds/bot/scenario_1/medicine_found_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative './shared/scenario_1/success'
 
 describe 'BotTelegram::FindMedsBot' do
   let!(:bot_record) { create :find_meds_bot }
@@ -9,100 +10,14 @@ describe 'BotTelegram::FindMedsBot' do
 
   context 'Scenario 1' do
     describe 'Medicine Found' do
-      let(:message_1) { build :telegram_message, text: 'Поиск лекарств' }
-      let(:message_2) { build :telegram_message, text: 'Финлепсин Ретард' }
-      let(:message_3) { build :telegram_message, text: 'NOVARTIS FARMA, S.p.A.' }
-      let(:message_4) { build :telegram_message, text: 'Таб.пролонгированного действия' }
-      let(:message_5) { build :telegram_message, text: '400 мг' }
+      include_context 'FindMeds Scenario 1 Success'
 
       it 'search medicine by name' do
-        find_meds_airtable_stub_request table: :drugs
-        find_meds_airtable_stub_request table: :medicines
-        find_meds_airtable_stub_request table: :companies
-        find_meds_airtable_stub_request table: :companies, id: 'receQeH2nuPmxUA7P'
-        find_meds_airtable_stub_request table: :companies, id: 'receQeH2nuPmxUA7L'
-        find_meds_airtable_stub_request table: :concentrations
-        find_meds_airtable_stub_request table: :active_components
-
-        stub_1 = send_message_stub_request body: {
-          chat_id: chat.telegram_chat_id,
-          text: 'Убедитесь, что название написано правильно'
-        }
-
-        bot_run :find_meds, bot_record: bot_record, message: message_1, chat: chat, message_object: message_object
-
-        expect(stub_1).to have_been_requested
-
-        stub_2 = send_message_stub_request body: {
-          chat_id: chat.telegram_chat_id,
-          text: 'Мы нашли лекараство. Лекарством какой фирмы вы пользуетесь?',
-          reply_markup: reply_markup(
-            [
-              'NOVARTIS FARMA, S.p.A.',
-              'МОСКОВСКИЙ ЭНДОКРИННЫЙ ЗАВОД, ФГУП'
-            ],
-            [
-              'В начало',
-              'Нужной фирмы нет'
-            ]
-          )
-        }
-
-        bot_run :find_meds, bot_record: bot_record, message: message_2, chat: chat, message_object: message_object
-
-        expect(stub_2).to have_been_requested
-
-        stub_3 = send_message_stub_request body: {
-          chat_id: chat.telegram_chat_id,
-          text: 'Какой лекарственной формулой вы пользуетесь?',
-          reply_markup: reply_markup(
-            [
-              'Таб.пролонгированного действия'
-            ],
-            [
-              'В начало',
-              'Нужной формы нет'
-            ]
-          )
-        }
-
-        bot_run :find_meds, bot_record: bot_record, message: message_3, chat: chat, message_object: message_object
-
-        expect(stub_3).to have_been_requested
-
-        stub_4 = send_message_stub_request body: {
-          chat_id: chat.telegram_chat_id,
-          text: 'Какая концентрация действующего вещества carbamazepine вам нужна?',
-          reply_markup: reply_markup(
-            [
-              '400 мг',
-              '100 мг'
-            ],
-            [
-              'В начало',
-              'Нужной концентрации нет'
-            ]
-          )
-        }
-
-        bot_run :find_meds, bot_record: bot_record, message: message_4, chat: chat, message_object: message_object
-
-        expect(stub_4).to have_been_requested
-
-        stub_5 = send_message_stub_request body: {
-          chat_id: chat.telegram_chat_id,
-          text: 'Это то лекарство, которое вы используете? Финлепсин Ретард "Teva Pharmaceutical Industries, Ltd." carbamazepine  концентрация 400 мг',
-          reply_markup: reply_markup(
-            [
-              'Да',
-              'Нет'
-            ]
-          )
-        }
-
-        bot_run :find_meds, bot_record: bot_record, message: message_5, chat: chat, message_object: message_object
-
-        expect(stub_5).to have_been_requested
+        push_search_medicine_button
+        type_existing_medicine
+        type_existing_company
+        type_existing_concentration
+        type_yes_on_reinforcement_question
       end
     end
   end
