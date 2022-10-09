1e3a0ed3c6fe7ae7500819944222b5849099137f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-10-10 01:29:17 +0400

    FindMeds. Test refactoring

--- spec/features/find_meds/bot/scenario_1/shared/scenario_1/failure.rb
+++ spec/features/find_meds/bot/scenario_1/shared/scenario_1/failure.rb
@@ -6,7 +6,7 @@ RSpec.shared_context 'FindMeds Scenario 1 Failure' do
   def type_not_existing_medicine
     find_meds_airtable_stub_request table: :drugs
 
-    stub_2 = send_message_stub_request body: {
+    stub = send_message_stub_request body: {
       chat_id: chat.telegram_chat_id,
       text: 'Увы, мы пока не знаем о таком лекарстве, позже мы добавим возможность искать и по действующим веществам, чтобы можно было найти дженерики даже если мы не знаем о том лекарстве, которое используете вы',
       reply_markup: reply_markup(
@@ -16,11 +16,11 @@ RSpec.shared_context 'FindMeds Scenario 1 Failure' do
 
     bot_run :find_meds, bot_record: bot_record, message: not_existing_medicine_message, chat: chat, message_object: message_object
 
-    expect(stub_2).to have_been_requested
+    expect(stub).to have_been_requested
   end
 
   def type_not_existing_company
-    stub_3 = send_message_stub_request body: {
+    stub = send_message_stub_request body: {
       chat_id: chat.telegram_chat_id,
       text: 'Увы, у нас пока нет информации о других компаниях, выпускающих это лекарство.',
       reply_markup: reply_markup(
@@ -30,11 +30,11 @@ RSpec.shared_context 'FindMeds Scenario 1 Failure' do
 
     bot_run :find_meds, bot_record: bot_record, message: no_needed_company_button_message, chat: chat, message_object: message_object
 
-    expect(stub_3).to have_been_requested
+    expect(stub).to have_been_requested
   end
 
   def type_feedback
-    stub_3 = send_message_stub_request body: {
+    stub = send_message_stub_request body: {
       chat_id: chat.telegram_chat_id,
       text: 'Мы приняли информацию!',
       reply_markup: reply_markup(
@@ -43,7 +43,7 @@ RSpec.shared_context 'FindMeds Scenario 1 Failure' do
     }
 
     feedback_id = FindMeds::Feedback.last.id + 1
-    stub_4 = send_message_stub_request(
+    stub = send_message_stub_request(
       body: {
         chat_id: ::BotTelegram::FindMedsBot::DEVELOPER_CHAT,
         text: "Мы получили новую обратную связь от пользователя. Посмотреть её можно здесь http://purple-magic.com/admin/records/#{feedback_id}?model=FindMeds::Feedback"
@@ -53,13 +53,13 @@ RSpec.shared_context 'FindMeds Scenario 1 Failure' do
 
     bot_run :find_meds, bot_record: bot_record, message: not_existing_medicine_message, chat: chat, message_object: message_object
 
-    expect(stub_3).to have_been_requested
-    expect(stub_4).to have_been_requested
+    expect(stub).to have_been_requested
+    expect(stub).to have_been_requested
     expect(FindMeds::Feedback.last.text).to eq not_existing_medicine_message.text
   end
 
   def push_to_beginning_button
-    stub_3 = send_message_stub_request body: {
+    stub = send_message_stub_request body: {
       chat_id: chat.telegram_chat_id,
       text: 'Выберите следующее действие',
       reply_markup: reply_markup([
@@ -69,6 +69,6 @@ RSpec.shared_context 'FindMeds Scenario 1 Failure' do
 
     bot_run :find_meds, bot_record: bot_record, message: to_beginning_button_message, chat: chat, message_object: message_object
 
-    expect(stub_3).to have_been_requested
+    expect(stub).to have_been_requested
   end
 end
--- spec/features/find_meds/bot/scenario_1/shared/scenario_1/success.rb
+++ spec/features/find_meds/bot/scenario_1/shared/scenario_1/success.rb
@@ -6,14 +6,14 @@ RSpec.shared_context 'FindMeds Scenario 1 Success' do
   let(:concentration_button_message) { build :telegram_message, text: '400 мг' }
 
   def push_search_medicine_button
-    stub_1 = send_message_stub_request body: {
+    stub = send_message_stub_request body: {
       chat_id: chat.telegram_chat_id,
       text: 'Убедитесь, что название написано правильно'
     }
 
     bot_run :find_meds, bot_record: bot_record, message: search_medicine_button_message, chat: chat, message_object: message_object
 
-    expect(stub_1).to have_been_requested
+    expect(stub).to have_been_requested
   end
   
   def type_existing_medicine
@@ -23,7 +23,7 @@ RSpec.shared_context 'FindMeds Scenario 1 Success' do
     find_meds_airtable_stub_request table: :companies, id: 'receQeH2nuPmxUA7P'
     find_meds_airtable_stub_request table: :companies, id: 'receQeH2nuPmxUA7L'
 
-    stub_2 = send_message_stub_request body: {
+    stub = send_message_stub_request body: {
       chat_id: chat.telegram_chat_id,
       text: 'Мы нашли лекараство. Лекарством какой фирмы вы пользуетесь?',
       reply_markup: reply_markup(
@@ -40,11 +40,11 @@ RSpec.shared_context 'FindMeds Scenario 1 Success' do
 
     bot_run :find_meds, bot_record: bot_record, message: medicine_button_message, chat: chat, message_object: message_object
 
-    expect(stub_2).to have_been_requested
+    expect(stub).to have_been_requested
   end
 
   def type_existing_company
-    stub_3 = send_message_stub_request body: {
+    stub = send_message_stub_request body: {
       chat_id: chat.telegram_chat_id,
       text: 'Какой лекарственной формулой вы пользуетесь?',
       reply_markup: reply_markup(
@@ -60,14 +60,14 @@ RSpec.shared_context 'FindMeds Scenario 1 Success' do
 
     bot_run :find_meds, bot_record: bot_record, message: company_button_message, chat: chat, message_object: message_object
 
-    expect(stub_3).to have_been_requested
+    expect(stub).to have_been_requested
   end
 
   def type_existing_concentration
     find_meds_airtable_stub_request table: :concentrations
     find_meds_airtable_stub_request table: :active_components
 
-    stub_4 = send_message_stub_request body: {
+    stub = send_message_stub_request body: {
       chat_id: chat.telegram_chat_id,
       text: 'Какая концентрация действующего вещества carbamazepine вам нужна?',
       reply_markup: reply_markup(
@@ -84,11 +84,11 @@ RSpec.shared_context 'FindMeds Scenario 1 Success' do
 
     bot_run :find_meds, bot_record: bot_record, message: form_button_message, chat: chat, message_object: message_object
 
-    expect(stub_4).to have_been_requested
+    expect(stub).to have_been_requested
   end
 
   def type_yes_on_reinforcement_question
-    stub_5 = send_message_stub_request body: {
+    stub = send_message_stub_request body: {
       chat_id: chat.telegram_chat_id,
       text: 'Это то лекарство, которое вы используете? Финлепсин Ретард "Teva Pharmaceutical Industries, Ltd." carbamazepine  концентрация 400 мг',
       reply_markup: reply_markup(
@@ -101,6 +101,6 @@ RSpec.shared_context 'FindMeds Scenario 1 Success' do
 
     bot_run :find_meds, bot_record: bot_record, message: concentration_button_message, chat: chat, message_object: message_object
 
-    expect(stub_5).to have_been_requested
+    expect(stub).to have_been_requested
   end
 end
