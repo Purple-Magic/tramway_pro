c6d887668a15983877c663df9e8930317b02835e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-10-24 04:13:57 +0400

    ProductEngine. Add products state

--- app/controllers/purple_magic/api/v1/products/jira_controller.rb
+++ app/controllers/purple_magic/api/v1/products/jira_controller.rb
@@ -1,9 +1,10 @@
 class PurpleMagic::Api::V1::Products::JiraController < PurpleMagic::Api::ApplicationController
   def create
-    begin
-      raise 'Jira'
-    rescue StandardError => error
-      Airbrake.notify error, params: params
+    jira_issue_id = params[:worklog][:issue_id]
+    task = Products::Task.find_by 'data -> jira_issue_id -> ?', jira_issue_id
+
+    unless task.present?
+      task = Products::Task.create! data: { jira_issue_id: jira_issue_id }
     end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,13 @@
 class Product < ApplicationRecord
   has_many :tasks, -> { order :id }, class_name: 'Products::Task', dependent: :destroy
   has_many :time_logs, through: :tasks, class_name: 'TimeLog'
+
+  aasm :product_state do
+    state :in_progress, initial: true
+    state :finished
+
+    event :finish do
+      transitions from: :in_progress, to: :finished
+    end
+  end
 end
