bfabf4e9e2636332fb85acb88d884be965a9ef7c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-10-24 04:20:23 +0400

    ProductEngine. Add worklog saving

--- app/controllers/purple_magic/api/v1/products/jira_controller.rb
+++ app/controllers/purple_magic/api/v1/products/jira_controller.rb
@@ -1,10 +1,16 @@
 class PurpleMagic::Api::V1::Products::JiraController < PurpleMagic::Api::ApplicationController
   def create
-    jira_issue_id = params[:worklog][:issue_id]
-    task = Products::Task.find_by 'data -> jira_issue_id -> ?', jira_issue_id
+    case params[:webhook_event] 
+    when 'worklog_created'
+      jira_issue_id = params[:worklog][:issue_id]
+      task = Products::Task.find_by 'data -> jira_issue_id -> ?', jira_issue_id
 
-    unless task.present?
-      task = Products::Task.create! data: { jira_issue_id: jira_issue_id }
+      unless task.present?
+        task = Products::Task.create! data: { jira_issue_id: jira_issue_id }, product_id: 15
+      end
+      task.time_logs.create! user_id: 43,
+        time_spent: params[:worklog][:time_spent],
+        comment: params[:worklog][:comment]
     end
   end
 end
