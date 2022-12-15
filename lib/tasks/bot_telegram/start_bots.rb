dc5e41f0b2099b64b6b1aed14c8ceb9c07680569
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-16 01:07:54 +0400

    Bot Engine. Add restarting monitoring for bots

--- lib/tasks/bot_telegram/start_bots.rb
+++ lib/tasks/bot_telegram/start_bots.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 require 'colorize'
+require_relative './leopold/notify'
+
+include BotTelegram::Leopold::Notify
 
 Bot.find_each do |bot|
   BotWorker.perform_async bot.id, bot.name
-  puts "#{bot.name} started".green
+  message = "#{bot.name} started"
+  puts message.green
+
+  send_notification_to_user :kalashnikovisme, message
 end
