410ff22178760df7c7c18a1f5c91d85e861a37a2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-22 03:32:47 +0400

    Podcast engine. Fix commands

--- app/services/podcasts/episodes/base_service.rb
+++ app/services/podcasts/episodes/base_service.rb
@@ -20,4 +20,24 @@ class Podcasts::Episodes::BaseService < ApplicationService
     end
     episode.save!
   end
+
+  def wait_for_file_rendered(output, file_type)
+    index = 0
+    until File.exist?(output)
+      sleep 1
+      index += 1
+      message = "#{file_type} file does not exist for #{index} seconds"
+      Rails.logger.info message
+    end
+  end
+
+  # :reek:UtilityFunction { enabled: false }
+  def write_logs(command)
+    "#{command} 2> #{Rails.root}/log/render-#{Rails.env}.log"
+  end
+  # :reek:UtilityFunction { enabled: true }
+
+  def move_to(temp_output, output)
+    "mv #{temp_output} #{output} && rm -f #{temp_output}"
+  end
 end
