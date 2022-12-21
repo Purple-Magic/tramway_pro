d0d08c8c01431e4497e60d46301083237c3837a0
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-22 03:07:58 +0400

    Podcast Engine. Some fixes

--- app/services/podcasts/episodes/base_service.rb
+++ app/services/podcasts/episodes/base_service.rb
@@ -7,4 +7,11 @@ class Podcasts::Episodes::BaseService < ApplicationService
   include Podcasts::Episodes::FilesManagement
   include Ffmpeg::CommandBuilder
   include Podcast::SoundProcessConcern
+
+  def log_command(episode, action, command)
+    line = [action, DateTime.now.strftime('%d.%m.%Y %H:%M:%S'), command].join(', ')
+    commands = (episode.render_data&.dig('commands') || []) + [line]
+    episode.render_data ? episode.render_data['commands'] = commands : episode.render_data = { commands: commands }
+    episode.save!
+  end
 end
--- app/services/podcasts/episodes/montage/filter_service.rb
+++ app/services/podcasts/episodes/montage/filter_service.rb
@@ -18,15 +18,15 @@ class Podcasts::Episodes::Montage::FilterService < Podcasts::Episodes::BaseServi
       output = "#{directory}/montage.mp3"
       temp_output = (output.split('.')[0..-2] + %w[temp mp3]).join('.')
 
-      build_and_run_command filename, output, temp_output
+      build_and_run_command episode.premontage_file.filename, output, temp_output
     end
 
     wait_for_file_rendered output, :montage
     update_file! output, :premontage_file
-    prepare!
+    episode.prepare!
   end
 
-  def build_and_run_command
+  def build_and_run_command(filename, output, temp_output)
     render_command = write_logs use_filters(input: filename, output: temp_output)
     move_command = move_to(temp_output, output)
     command = "#{render_command} && #{move_command}"
