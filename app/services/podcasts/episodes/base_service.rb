d91d0eed7e93a6a0e09788aae261076bed61e25d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-22 03:27:42 +0400

    Podcast Engine. Fix filtering

--- app/services/podcasts/episodes/base_service.rb
+++ app/services/podcasts/episodes/base_service.rb
@@ -6,12 +6,18 @@ class Podcasts::Episodes::BaseService < ApplicationService
   include Podcasts::Episodes::CommandsManagement
   include Podcasts::Episodes::FilesManagement
   include Ffmpeg::CommandBuilder
-  include Podcast::SoundProcessConcern
 
   def log_command(episode, action, command)
     line = [action, DateTime.now.strftime('%d.%m.%Y %H:%M:%S'), command].join(', ')
     commands = (episode.render_data&.dig('commands') || []) + [line]
     episode.render_data ? episode.render_data['commands'] = commands : episode.render_data = { commands: commands }
+    episode.save!
+  end
+
+  def update_file!(episode, output, file_type)
+    File.open(output) do |std_file|
+      episode.public_send "#{file_type}=", std_file
+    end
     episode.save!
   end
 end
