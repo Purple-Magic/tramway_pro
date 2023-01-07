1c6a605f71ce92c8043588b68fbf9e2a5d30ffa9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-01-07 19:55:56 +0400

    Podcast Engine. Add notification about video rendering

--- app/workers/podcasts/youtube_publish_worker.rb
+++ app/workers/podcasts/youtube_publish_worker.rb
@@ -3,24 +3,10 @@
 class Podcasts::YoutubePublishWorker < ApplicationWorker
   sidekiq_options queue: :podcast
 
-  include BotTelegram::Leopold::Notify
-
   def perform(id)
     episode = Podcast::Episode.find id
-
-    account = Yt::Account.new access_token: ::Youtube::Account.last.access_token
-
-    video = account.upload_video(
-      episode.full_video.path,
-      title: episode.public_title
-      # description: Podcast::Youtube::VideoDecorator.new(episode).description
-    )
-
-    instance = episode.instances.create! service: :youtube, link: "https://www.youtube.com/watch?v=#{video.id}"
-    ::Shortener::ShortenedUrl.generate(instance.link, owner: instance)
-    instance = Podcast::Episodes::InstanceDecorator.new instance
-
-    send_notification_to_chat episode.podcast.chat_id, notification(:youtube_publish, :success, link: instance.link)
+    
+    Podcasts::Youtube::PublishService.new(episode).call
   rescue StandardError => error
     Rails.env.development? ? puts(error) : Airbrake.notify(error)
   end
