896630ffec9e650d78e6dd00d8bbdfe3ea3fd864
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-02-09 17:08:46 +0400

    Podcast Engine. Upgrade star forms

--- app/forms/admin/podcast/star_form.rb
+++ app/forms/admin/podcast/star_form.rb
@@ -8,14 +8,62 @@ class Admin::Podcast::StarForm < Tramway::ApplicationForm
   def initialize(object)
     super(object).tap do
       form_properties podcast: :association,
-        nickname: :string,
-        first_name: :string,
-        last_name: :string,
-        link: :string,
-        vk: :string,
-        twitter: :string,
-        telegram: :string,
-        instagram: :string
+        nickname: {
+          type: :string,
+          input_options: {
+            placeholder: I18n.t('placeholders.podcast/star.nickname'),
+            hint: I18n.t('hints.podcast/star.nickname')
+          }
+        },
+        first_name: {
+          type: :string,
+          input_options: {
+            placeholder: I18n.t('placeholders.podcast/star.first_name'),
+            hint: I18n.t('hints.podcast/star.first_name')
+          }
+        },
+        last_name: {
+          type: :string,
+          input_options: {
+            placeholder: I18n.t('placeholders.podcast/star.last_name'),
+            hint: I18n.t('hints.podcast/star.last_name')
+          }
+        },
+        link: {
+          type: :string,
+          input_options: {
+            placeholder: I18n.t('placeholders.podcast/star.link'),
+            hint: I18n.t('hints.podcast/star.link')
+          }
+        },
+        twitter: {
+          type: :string,
+          input_options: {
+            placeholder: I18n.t('placeholders.podcast/star.twitter'),
+            hint: I18n.t('hints.podcast/star.twitter')
+          }
+        },
+        telegram: {
+          type: :string,
+          input_options: {
+            placeholder: I18n.t('placeholders.podcast/star.telegram'),
+            hint: I18n.t('hints.podcast/star.telegram')
+          }
+        },
+        instagram: {
+          type: :string,
+          input_options: {
+            placeholder: I18n.t('placeholders.podcast/star.instagram'),
+            hint: I18n.t('hints.podcast/star.instagram')
+          }
+        },
+        vk: {
+          type: :string,
+          input_options: {
+            placeholder: I18n.t('placeholders.podcast/star.vk'),
+            hint: I18n.t('hints.podcast/star.vk')
+          }
+        }
     end
   end
 end
