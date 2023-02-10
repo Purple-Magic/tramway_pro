ce0128513d06f29a24f6143b0a149931dfa995ea
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-02-10 04:28:45 +0400

    Fix manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,4 +3,11 @@
 //= link_directory ../stylesheets .css
 //= link tramway/application.css
 //= link tramway/application.js
+//= link tramway/landing/application.css
+//= link tramway/landing/application.js
+//= link tramway/conference/application.css
+//= link tramway/conference/application.js
+//= link tramway/event/application.js
+//= link tramway/news/application.css
+
 
