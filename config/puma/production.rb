ea60ceda27e01942f7d4dcb3cc54f61312294f06
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-02-10 17:08:10 +0400

    Fix puma config

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,18 +3,19 @@
 rails_env = "production"
 environment rails_env
 
-app_dir = "srv/tramway_pro/shared" # Update me with your root rails app path
+app_dir = "/srv/tramway_pro" # Update me with your root rails app path
+shared_dir "#{app_dir}/shared/"
 
-bind  "unix://#{app_dir}/puma.sock"
-pidfile "#{app_dir}/puma.pid"
-state_path "#{app_dir}/puma.state"
-directory "#{app_dir}/"
+bind  "unix://#{shared_dir}/puma.sock"
+pidfile "#{shared_dir}/puma.pid"
+state_path "#{shared_dir}/puma.state"
+directory "#{app_dir}/current"
 
-stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 workers 2
 threads 1,2
 
-activate_control_app "unix://#{app_dir}/pumactl.sock"
+activate_control_app "unix://#{shared_dir}/pumactl.sock"
 
 prune_bundler
