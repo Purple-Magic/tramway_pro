4a57c30297c9f2a5cdf0898918f731ba2a4bca54
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-02-11 04:19:49 +0400

    Fix bot_listener

--- app/workers/bot_worker.rb
+++ app/workers/bot_worker.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../../lib/tasks/bot_telegram/bot_listener'
+require 'bot_telegram/bot_listener'
 
 class BotWorker < ApplicationWorker
   sidekiq_options queue: :bot
