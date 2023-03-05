accf1433465983d4006c6a9efc70e4fe62d89842
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-03-05 11:02:28 +0400

    Fix deploy

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.logger = Logger.new('./log/sidekiq.log')
+  # config.logger = Logger.new('./log/sidekiq.log')
   config.logger.level = ::Logger::INFO
   # Sidekiq::Cron::Job.load_from_hash YAML.load_file 'config/sidekiq-schedule.yml'
 
