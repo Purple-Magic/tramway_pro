b4ce4e3e5e1e2bd984aadea997f429da17c6cc83
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-03-05 09:32:30 +0400

    Fix deploy

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@
 Sidekiq.configure_server do |config|
   config.logger = Logger.new('./log/sidekiq.log')
   config.logger.level = ::Logger::INFO
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file 'config/sidekiq-schedule.yml'
+  # Sidekiq::Cron::Job.load_from_hash YAML.load_file 'config/sidekiq-schedule.yml'
 
   config.redis = {
     url: 'redis://redis:6379/1',
